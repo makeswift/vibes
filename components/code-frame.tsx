42d587e27254ba5533709835222216f5ed518f67
Alan Pledger <apledger3@gmail.com>
2024-02-23 17:54:53 -0500

    feat: add tabs

--- components/code-frame.tsx
+++ components/code-frame.tsx
@@ -1,1 +1,8 @@
-export function CodeFrame() {}
+export function CodeFrame({ children, filename = 'test.tsx' }) {
+  return (
+    <div className="[&>pre]:my-0">
+      <div className="">{filename}</div>
+      {children}
+    </div>
+  )
+}
