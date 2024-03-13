5e3e38cc45c9757f06874d820540ae4f441dc979
Andrew Reifman <andrewr23@gmail.com>
2024-03-13 17:19:05 -0400

    feat: add table of contents component

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,84 @@
 import Image from 'next/image'
 
 import { Button } from '@/components/ui/button'
+import { Heading, TableOfContents } from '@/components/ui/table-of-contents'
 
 export default function Home() {
   return (
-    <main className="vibes-prose prose min-h-screen w-full p-16">
-      <h1>This is a main header</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
-        aliquet nibh nec urna. In <code>nisi neque</code>, aliquet vel, dapibus id, mattis vel,
-        nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget
-        blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
-      </p>
+    <div className="mx-auto max-w-5xl">
+      <div className="mb-10 h-96 w-full border border-black" />
+      <main className="flex items-stretch gap-x-12">
+        <div className="vibes-prose prose !w-full flex-1">
+          <h1>This is a main header</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit.
+            Pellentesque aliquet nibh nec urna. In <code>nisi neque</code>, aliquet vel, dapibus id,
+            mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero
+            sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
+            potenti.
+          </p>
 
-      <h2>This is a secondary header</h2>
-      <p>
-        Lorem <a href="/">ipsum dolor sit amet</a>, consectetuer adipiscing elit. Phasellus
-        hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id,
-        mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales
-        leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
-      </p>
+          <Heading as="h2">This is a secondary header</Heading>
+          <p>
+            Lorem <a href="/">ipsum dolor sit amet</a>, consectetuer adipiscing elit. Phasellus
+            hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id,
+            mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero
+            sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
+            potenti.
+          </p>
 
-      <div className="flex gap-1">
-        <Button variant="default" size="default">
-          Testing
-        </Button>
+          <div className="flex gap-1">
+            <Button variant="default" size="default">
+              Testing
+            </Button>
 
-        <Button variant="ghost" size="default">
-          Testing
-        </Button>
-      </div>
+            <Button variant="ghost" size="default">
+              Testing
+            </Button>
+          </div>
 
-      <h3>This is a smaller subheader</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
-        aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed
-        pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc
-        tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
-      </p>
+          <Heading as="h2">This is long heading with a lot of text</Heading>
 
-      <ul>
-        <li>This is a list item</li>
-        <li>This is a list item</li>
-        <li>This is a list item</li>
-        <li>This is a list item</li>
-      </ul>
+          <Heading as="h3">This is a smaller subheader</Heading>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit.
+            Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel,
+            nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget
+            blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+          </p>
 
-      <h3>Some code</h3>
-      <pre>
-        <code>This is some example code</code>
-      </pre>
-    </main>
+          <Heading as="h3">This is a list of stuff</Heading>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit.
+            Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel,
+            nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget
+            blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+          </p>
+
+          <ul>
+            <li>This is a list item</li>
+            <li>This is a list item</li>
+            <li>This is a list item</li>
+            <li>This is a list item</li>
+          </ul>
+
+          <Heading as="h2">This is a secondary header</Heading>
+          <p>
+            Lorem <a href="/">ipsum dolor sit amet</a>, consectetuer adipiscing elit. Phasellus
+            hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id,
+            mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero
+            sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
+            potenti.
+          </p>
+
+          <Heading as="h3">This is some code</Heading>
+          <pre>
+            <code>This is some example code</code>
+          </pre>
+        </div>
+
+        <TableOfContents />
+      </main>
+    </div>
   )
 }
---
+++ components/ui/table-of-contents.tsx
@@ -0,0 +1,100 @@
+'use client'
+
+import React, { use } from 'react'
+
+import { cn } from '@/lib/utils'
+
+type HeadingType = { id: string; text: string; level: number }
+function useHeadings() {
+  const [headings, setHeadings] = React.useState<HeadingType[]>([])
+  React.useEffect(() => {
+    const elements = Array.from(document.querySelectorAll('h2, h3, h4, h5, h6'))
+      .filter(element => element.id)
+      .map(element => ({
+        id: element.id,
+        text: element.textContent ?? '',
+        level: Number(element.tagName.substring(1)),
+      }))
+    setHeadings(elements)
+  }, [])
+  return headings
+}
+
+function getId(children: string) {
+  return children
+    .split(' ')
+    .map(word => word.toLowerCase())
+    .join('-')
+}
+
+type HeadingProps = {
+  children: string
+  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  id?: string
+}
+
+export function Heading({ children, id, as: Element, ...props }: HeadingProps) {
+  const theId = id ?? getId(children)
+  return (
+    <Element id={theId} {...props}>
+      {children}
+    </Element>
+  )
+}
+
+function useScrollSpy(ids: string[], options: IntersectionObserverInit) {
+  const [activeId, setActiveId] = React.useState<string>()
+  const observer = React.useRef<IntersectionObserver>()
+  React.useEffect(() => {
+    const elements = ids.map(id => document.getElementById(id))
+    observer.current?.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry?.isIntersecting) {
+          setActiveId(entry.target.id)
+        }
+      })
+    }, options)
+    elements.forEach(el => {
+      if (el) {
+        observer.current?.observe(el)
+      }
+    })
+    return () => observer.current?.disconnect()
+  }, [ids, options])
+  return activeId
+}
+
+// Now, the function that renders it all
+export function TableOfContents() {
+  const headings = useHeadings()
+  const activeId = useScrollSpy(
+    headings.map(({ id }) => id),
+    { rootMargin: '0% 0% -25% 0%' }
+  )
+  return (
+    <div className="hidden xl:block">
+      <nav className="sticky top-28 w-48">
+        <h2 className="mb-3 text-sm font-bold">On this page</h2>
+        <ul>
+          {headings.map(heading => (
+            <li key={heading.id}>
+              <a
+                className={cn(
+                  'block py-1 text-sm font-light leading-normal text-black/50 transition-colors hover:text-black',
+                  activeId === heading.id && 'text-black'
+                )}
+                style={{
+                  marginLeft: `${heading.level - 2}em`,
+                }}
+                href={`#${heading.id}`}
+              >
+                {heading.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  )
+}
