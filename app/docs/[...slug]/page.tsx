375d769f1d5c64a6a93aa65c3c82725ad15ed089
drewpledger <drew@makeswift.com>
2024-03-17 23:31:20 -0400

    feat: dynamic imports per page

--- app/docs/[...slug]/page.tsx
+++ app/docs/[...slug]/page.tsx
@@ -1,15 +1,62 @@
 // vibes.site/docs/linearesque/button
 import { MDXRemote } from 'next-mdx-remote/rsc'
+import dynamic from 'next/dynamic'
 import { permanentRedirect } from 'next/navigation'
 
 import { readFile } from 'fs/promises'
+import { MDXComponents } from 'mdx/types'
 
+const imports = {
+  linearesque: {
+    card: [
+      {
+        name: 'Button',
+        import: dynamic(() =>
+          import('@/components/ui/button').then(module => ({ default: module.Button }))
+        ),
+      },
+      {
+        name: 'Tabs',
+        import: dynamic(() =>
+          import('@/components/ui/tabs').then(module => ({ default: module.Tabs }))
+        ),
+      },
+      {
+        name: 'TabsContent',
+        import: dynamic(() =>
+          import('@/components/ui/tabs').then(module => ({ default: module.TabsContent }))
+        ),
+      },
+      {
+        name: 'TabsList',
+        import: dynamic(() =>
+          import('@/components/ui/tabs').then(module => ({ default: module.TabsList }))
+        ),
+      },
+      {
+        name: 'TabsTrigger',
+        import: dynamic(() =>
+          import('@/components/ui/tabs').then(module => ({ default: module.TabsTrigger }))
+        ),
+      },
+    ],
+  },
+}
 // params { slug: '/controls/checkbox' }
-export default async function Page({ params }: { params: { slug?: [string] } }) {
+export default async function Page({ params }: { params: { slug?: string[] } }) {
   const file = await readFile(process.cwd() + '/mdx/' + params.slug?.join('/') + '.mdx').catch(() =>
     permanentRedirect('/404')
   )
 
-  // otherwise, compile and render the MDX.
-  return <MDXRemote source={file} />
+  const vibe = params.slug?.[0] as keyof typeof imports
+  const page = params.slug?.[1] as keyof (typeof imports)[typeof vibe]
+
+  const components = {} as MDXComponents
+
+  for (const { name, import: importPromise } of imports[vibe][page] || []) {
+    const component = await importPromise
+    components[name] = component
+  }
+
+  return <MDXRemote source={file} components={components} />
 }
