be4408b2470d5225ce6bdcaa494273b48ce2987f
Alan Pledger <apledger3@gmail.com>
2024-04-02 13:01:53 -0400

    feat: add icons cli command

---
+++ icons/generated/Sun.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import type { SVGProps } from 'react'
+
+const SvgSun = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
+    <circle cx={12} cy={12} r={6.5} fill="#FAD880" stroke="#07090D" />
+    <path
+      fill="#07090D"
+      fillRule="evenodd"
+      d="M12.5 3V0h-1v3zm0 21v-3h-1v3zm5.933-22.142-1.5 2.598-.866-.5 1.5-2.598zm-12 20.784 1.5-2.598-.866-.5-1.5 2.598zm16.21-16.209-2.599 1.5-.5-.866 2.598-1.5zm-20.785 12 2.598-1.5-.5-.866-2.598 1.5zM24 12.5h-3v-1h3zm-24 0h3v-1H0zm22.142 5.933-2.598-1.5.5-.866 2.598 1.5zm-20.784-12 2.598 1.5.5-.866-2.598-1.5zm16.209 16.21-1.5-2.599.866-.5 1.5 2.598zm-12-20.785 1.5 2.598.866-.5-1.5-2.598z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default SvgSun
---
+++ icons/generated/UpDownArrows.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import type { SVGProps } from 'react'
+
+const SvgUpDownArrows = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" {...props}>
+    <path stroke="#07090D" strokeWidth={1.5} d="m4 5.5 4-4 4 4M12 10.5l-4 4-4-4" />
+  </svg>
+)
+export default SvgUpDownArrows
