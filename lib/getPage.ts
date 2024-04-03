5d7bd9ed631d62ff47d67b50800a7154613ac0f0
Alan Pledger <apledger3@gmail.com>
2024-04-03 14:49:00 -0400

    refactor: move to mdx

--- lib/getPage.ts
+++ lib/getPage.ts
@@ -2,5 +2,5 @@ import { readFile } from 'fs/promises'
 import path from 'path'
 
 export function getPage(pathname: string) {
-  return readFile(path.resolve(path.join('docs', `${pathname}.mdx`)), 'utf8')
+  return readFile(path.resolve(path.join('mdx', `${pathname}.mdx`)), 'utf8')
 }
