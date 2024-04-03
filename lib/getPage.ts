b1c57990c033c38ffc9bc08e985ab13598fef4f5
Alan Pledger <apledger3@gmail.com>
2024-04-03 14:31:37 -0400

    fix: remove async

--- lib/getPage.ts
+++ lib/getPage.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'fs/promises'
 import path from 'path'
 
-export async function getPage(pathname: string) {
+export function getPage(pathname: string) {
   return readFile(path.join(process.cwd(), 'docs', `${pathname}.mdx`))
 }
