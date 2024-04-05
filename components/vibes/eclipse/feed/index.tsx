b5ce9844d263450c1acdcbfcb050f6a5167c2715
Alan Pledger <apledger3@gmail.com>
2024-04-05 09:18:57 -0400

    fix: types in feed

--- components/vibes/eclipse/feed/index.tsx
+++ components/vibes/eclipse/feed/index.tsx
@@ -21,7 +21,7 @@ type Item = {
 
 type Props = {
   className?: string
-  items?: Item[]
+  items: Item[]
   paginate?: boolean
   defaultShown?: number
   paginationAmount?: number
@@ -46,8 +46,8 @@ const Feed = forwardRef(function Feed(
   }, [defaultShown])
 
   return (
-    <div ref={ref} className={clsx(className, '@container flex flex-col items-center')}>
-      <div className="@sm:gap-x-8 @sm:gap-y-8 @3xl:gap-x-10 @3xl:gap-y-10 grid w-full grid-cols-12 gap-x-0 gap-y-6">
+    <div ref={ref} className={clsx(className, 'flex flex-col items-center @container')}>
+      <div className="grid w-full grid-cols-12 gap-x-0 gap-y-6 @sm:gap-x-8 @sm:gap-y-8 @3xl:gap-x-10 @3xl:gap-y-10">
         {items?.slice(0, Math.min(page, items.length)).map((item, index) => (
           <Link
             href={item.link?.href ?? ''}
@@ -56,14 +56,14 @@ const Feed = forwardRef(function Feed(
             className={clsx(
               'card group z-0 overflow-hidden transition duration-500 hover:bg-primary/10 hover:ring-primary/50',
               index === 0
-                ? '@xl:flex-row col-span-12 flex flex-col'
-                : '@lg:col-span-6 @4xl:col-span-4 col-span-12'
+                ? 'col-span-12 flex flex-col @xl:flex-row'
+                : 'col-span-12 @lg:col-span-6 @4xl:col-span-4'
             )}
           >
             <div
               className={clsx(
                 'relative aspect-video bg-background',
-                index === 0 ? '@lg:h-full @xl:w-1/2 @5xl:w-2/3 w-full' : 'w-full'
+                index === 0 ? 'w-full @lg:h-full @xl:w-1/2 @5xl:w-2/3' : 'w-full'
               )}
             >
               {item.thumbnail && (
@@ -79,8 +79,8 @@ const Feed = forwardRef(function Feed(
               className={clsx(
                 'before:rounded-circle before:duration-[400ms] relative flex flex-1 flex-col gap-y-6 p-5 text-foreground before:absolute before:left-1/2 before:top-full before:-z-10 before:aspect-square before:w-full before:-translate-x-1/2 before:scale-50 before:bg-primary before:opacity-0 before:transition-[opacity,transform] before:ease-linear group-hover:before:scale-100 group-hover:before:opacity-50',
                 index === 0
-                  ? '@xl:gap-y-10 @xl:p-10 @2xl:gap-y-12 @4xl:gap-y-16 @4xl:p-12 justify-between before:-translate-y-20 before:blur-[80px]'
-                  : '@xl:gap-y-8 @xl:p-7 @xl:pt-6 before:-translate-y-10 before:blur-3xl'
+                  ? 'justify-between before:-translate-y-20 before:blur-[80px] @xl:gap-y-10 @xl:p-10 @2xl:gap-y-12 @4xl:gap-y-16 @4xl:p-12'
+                  : 'before:-translate-y-10 before:blur-3xl @xl:gap-y-8 @xl:p-7 @xl:pt-6'
               )}
             >
               <div>
@@ -98,8 +98,8 @@ const Feed = forwardRef(function Feed(
                     className={clsx(
                       'text-xl font-medium',
                       index === 0
-                        ? '@xl:text-2xl @2xl:text-3xl @4xl:text-4xl !leading-tight'
-                        : '@4xl:text-2xl leading-normal'
+                        ? '!leading-tight @xl:text-2xl @2xl:text-3xl @4xl:text-4xl'
+                        : 'leading-normal @4xl:text-2xl'
                     )}
                   >
                     {item.title}
@@ -109,7 +109,7 @@ const Feed = forwardRef(function Feed(
                 {item.description && (
                   <p
                     className={clsx(
-                      'text @xl:line-clamp-4 @2xl:mt-5 @2xl:line-clamp-5 @2xl:text-lg mt-3 hidden font-light leading-normal opacity-70',
+                      'text mt-3 hidden font-light leading-normal opacity-70 @xl:line-clamp-4 @2xl:mt-5 @2xl:line-clamp-5 @2xl:text-lg',
                       index !== 0 && '!hidden'
                     )}
                   >
@@ -128,9 +128,9 @@ const Feed = forwardRef(function Feed(
         ))}
       </div>
 
-      {paginate && page < items?.length && (
+      {paginate && page < items.length && (
         <button
-          className="button button-muted button-md @lg:mt-14 mt-10"
+          className="button button-muted button-md mt-10 @lg:mt-14"
           onClick={() => setPage(p => p + paginationAmount)}
         >
           {paginateText}
