205553e8a23c74d677d691a6ca2e005102e3abb1
Alan Pledger <apledger3@gmail.com>
2024-06-05 15:45:42 -0400

    fix: sticker interactions for touch

--- components/ui/sticker/draggable-sticker.tsx
+++ components/ui/sticker/draggable-sticker.tsx
@@ -2,19 +2,28 @@
 
 import { ComponentPropsWithoutRef, useCallback, useRef, useState } from 'react'
 
+import clsx from 'clsx'
+
 import Sticker from './sticker'
+import styles from './sticker.module.css'
 
 export default function DraggableSticker(props: ComponentPropsWithoutRef<typeof Sticker>) {
   const pointerStart = useRef([0, 0])
   const positionStart = useRef([0, 0])
   const [position, setPosition] = useState([0, 0])
-  const onMove = useCallback((e: MouseEvent) => {
+  const [active, setActive] = useState(false)
+  const onPointerMove = useCallback((e: PointerEvent) => {
     setPosition([
       positionStart.current[0] + e.clientX - pointerStart.current[0],
       positionStart.current[1] + e.clientY - pointerStart.current[1],
     ])
+  }, [])
 
-    window.addEventListener('pointerup', () => window.removeEventListener('pointermove', onMove))
+  const onTouchMove = useCallback((e: TouchEvent) => {
+    setPosition([
+      positionStart.current[0] + e.touches[0].clientX - pointerStart.current[0],
+      positionStart.current[1] + e.touches[0].clientY - pointerStart.current[1],
+    ])
   }, [])
 
   return (
@@ -29,9 +38,29 @@ export default function DraggableSticker(props: ComponentPropsWithoutRef<typeof 
           pointerStart.current = [e.clientX, e.clientY]
           positionStart.current = position
 
-          window.addEventListener('pointermove', onMove)
+          setActive(true)
+
+          window.addEventListener('pointermove', onPointerMove)
+          window.addEventListener('pointerup', () => {
+            setActive(false)
+
+            window.removeEventListener('pointermove', onPointerMove)
+          })
         }}
-        className="scale-75 md:scale-100 xl:scale-125"
+        onTouchStart={e => {
+          pointerStart.current = [e.touches[0].clientX, e.touches[0].clientY]
+          positionStart.current = position
+
+          setActive(true)
+
+          window.addEventListener('touchmove', onTouchMove)
+          window.addEventListener('touchend', () => {
+            setActive(false)
+
+            window.removeEventListener('touchmove', onTouchMove)
+          })
+        }}
+        className={clsx('scale-75 md:scale-100 xl:scale-125', active && styles['active'])}
       />
     </div>
   )
