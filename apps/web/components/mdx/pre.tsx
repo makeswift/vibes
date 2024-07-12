4564045c292390bb76fa674c1f20a1c5b7624361
Alan Pledger <apledger3@gmail.com>
2024-07-12 16:56:16 -0400

    fix: remove copy button from pre

--- apps/web/components/mdx/pre.tsx
+++ apps/web/components/mdx/pre.tsx
@@ -1,13 +1,9 @@
 import { ComponentPropsWithoutRef } from 'react'
 
-import { CopyButton } from '../ui/copy-button'
-
 export default function Pre(props: ComponentPropsWithoutRef<'pre'>) {
   return (
     <div className="pattern-shadow-sm pattern-shadow group relative border border-foreground bg-background last:[&>pre]:!mb-0">
       <pre {...props} />
-
-      <CopyButton className="absolute right-3 top-2.5 z-20 opacity-0 transition-opacity group-hover:opacity-100" />
     </div>
   )
 }
