34ec2ad1f0234b2f2567349aa560ad3b0b2906b8
Mohammed Elhadi Baci <elhadi98@gmail.com>
2024-07-23 13:43:55 +0100

    initial setup for 2px

---
+++ apps/web/vibes/2px/examples.ts
@@ -0,0 +1,5 @@
+import { lazy } from 'react'
+
+import { Components } from '@/vibes/schema'
+
+export const examples = [] satisfies Components
---
+++ apps/web/vibes/2px/navigation.ts
@@ -0,0 +1,3 @@
+import { Navigation } from '@/vibes/schema'
+
+export const navigation = [] satisfies Navigation
