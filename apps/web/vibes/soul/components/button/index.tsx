700c8d31b07acc884ba4f3262db936581d5dbb14
Joseph Cortez <joseph@monogram.io>
2024-07-22 21:20:01 -0400

    button component (wip)

--- apps/web/vibes/soul/components/button/index.tsx
+++ apps/web/vibes/soul/components/button/index.tsx
@@ -8,21 +8,13 @@ import clsx from 'clsx'
 export interface Props {
   className?: string
   link?: { href: string; target?: string }
-  variant?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary' | 'tertiary'
   size?: 'default' | 'small'
   children?: ReactNode
-  borderGlow?: boolean
 }
 
 export const Button = forwardRef(function Button(
-  {
-    className,
-    link,
-    variant = 'primary',
-    size = 'default',
-    children = 'Button',
-    borderGlow = true,
-  }: Props,
+  { className, link, variant = 'primary', size = 'default', children = 'Button' }: Props,
   ref: Ref<HTMLAnchorElement>
 ) {
   return (
@@ -30,33 +22,22 @@ export const Button = forwardRef(function Button(
       ref={ref}
       className={clsx(
         className,
-        'group relative z-0 overflow-hidden rounded-full p-[1px] text-center leading-normal text-foreground shadow-xl shadow-black/25',
-        link?.href === '#' && 'pointer-events-none'
+        'group relative shrink-0 overflow-hidden rounded-full text-center font-medium leading-normal before:absolute before:left-0 before:top-0 before:z-0 before:h-14 before:w-full before:-translate-x-[110%] before:rounded-[28px] before:transition-transform before:duration-300 before:ease-in-out hover:before:translate-x-0',
+        link?.href === '#' && 'pointer-events-none',
+        {
+          primary: 'bg-primary text-background before:bg-accent',
+          secondary: 'bg-foreground text-background before:bg-background hover:text-foreground',
+          tertiary: 'bg-background text-foreground before:bg-foreground hover:text-background',
+        }[variant],
+        {
+          default: 'px-6 py-4 text-base',
+          small: 'px-4 py-2 text-sm',
+        }[size]
       )}
       href={link?.href ?? '#'}
       target={link?.target}
     >
-      <div
-        className={clsx(
-          'absolute left-1/2 top-1/2 -z-10 aspect-square w-[200%] origin-top-left animate-rotate bg-[conic-gradient(var(--tw-gradient-stops))] from-transparent via-transparent to-primary ease-linear',
-          borderGlow ? 'opacity-100' : 'opacity-0'
-        )}
-      />
-      <div
-        className={clsx(
-          'inline-flex items-center justify-center gap-x-3 overflow-hidden rounded-full bg-black leading-normal text-foreground ring-1 ring-foreground/20 transition duration-700 before:absolute before:bottom-0 before:left-1/2 before:-z-0 before:h-full before:w-full before:-translate-x-1/2 before:translate-y-1/2 before:rounded-full before:blur-lg before:transition-all before:duration-700 before:ease-in-out hover:ring-foreground/40 before:hover:scale-110 hover:before:opacity-40',
-          {
-            primary: 'before:bg-primary before:opacity-25',
-            secondary: 'before:opacity-0',
-          }[variant],
-          {
-            default: 'px-6 py-3 text-sm',
-            small: 'px-4 py-2 text-sm',
-          }[size]
-        )}
-      >
-        <span className="shrink-0 text-foreground">{children}</span>
-      </div>
+      <span className="relative z-10">{children}</span>
     </Link>
   )
 })
