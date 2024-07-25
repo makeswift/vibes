ac807ab5d27634615befb35e6cafb2b0374cd792
Andrew Reifman <andrewr23@gmail.com>
2024-07-25 12:36:05 -0400

    add features section to page

--- apps/web/icons/generated/Check.tsx
+++ apps/web/icons/generated/Check.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react'
 import type { SVGProps } from 'react'
 
-const SvgCheck = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={28} height={28} fill="none" {...props}>
-    <path
-      fill="#fff"
-      d="M12.5 26H10L0 14.068h5.114L10 20.077q.343.426.514 1.15.215.768.257.938.172.468.472.469.171 0 .3-.17a.53.53 0 0 0 .086-.427q-.043-.171-.215-.767a6 6 0 0 1-.128-1.279L23 2h5z"
-    />
+import clsx from 'clsx'
+
+const SvgCheck = ({ className }: { className?: string }, props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="none"
+    className={clsx('stroke-foreground', className)}
+    {...props}
+  >
+    <path d="M7 11.5L14.5 4" strokeWidth="1.5" strokeLinecap="square" strokeDasharray="1.5 3.25" />
+    <path d="M2 8.5L6 12.5" strokeWidth="1.5" strokeLinecap="square" />
   </svg>
 )
 export default SvgCheck
