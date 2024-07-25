a5d348e7ef39a6e3181b928475b36db7e1193d08
Mohammed Elhadi Baci <elhadi98@gmail.com>
2024-07-25 15:36:44 +0100

    add 2px navigation placeholder

--- apps/web/vibes/2px/navigation.ts
+++ apps/web/vibes/2px/navigation.ts
@@ -1,3 +1,15 @@
 import { Navigation } from '@/vibes/schema'
 
-export const navigation = [] satisfies Navigation
+export const navigation = [
+  {
+    title: 'Components',
+    pages: [
+      {
+        title: 'Button',
+        slug: 'button',
+        file: 'docs/button.mdx',
+        component: 'button',
+      },
+    ],
+  },
+] satisfies Navigation
