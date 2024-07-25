abdebb4cf81438fe269774d325a296ed1b3869f1
Andrew Reifman <andrewr23@gmail.com>
2024-07-25 12:09:18 -0400

    styling vibe switcher content

--- apps/web/components/navigation/vibe-select.tsx
+++ apps/web/components/navigation/vibe-select.tsx
@@ -44,13 +44,55 @@ export function VibeSelect({ vibeSlug }: Props) {
           />
         </Button>
       </SheetTrigger>
-      <SheetContent side="top" className="z-20">
-        <div className="mx-auto gap-5 px-5 xl:container lg:flex xl:px-8 ">
+      <SheetContent side="top" className="z-20 focus:outline-none">
+        <div className="mx-auto grid grid-cols-1 gap-x-6 gap-y-8 xl:container md:grid-cols-2 md:gap-y-10 lg:gap-x-8 lg:py-2 xl:px-8 2xl:grid-cols-3">
           {navigation.vibes.map(vibe => (
-            <Link key={vibe.slug} href={`/docs/${vibe.slug}`} className="flex-1">
-              <img src="" alt="" className="mb-4 aspect-video bg-contrast-100" />
-              <div className="mb-2 flex gap-x-2 text-xl font-bold">{vibe.name}</div>
-              <p className="text-foreground">
+            <Link
+              key={vibe.slug}
+              href={`/docs/${vibe.slug}`}
+              className="group ring-primary ring-offset-8 focus:outline-none focus-visible:ring-2"
+            >
+              <div className="marching-ants mb-4 border border-dashed border-transparent [animation-play-state:paused] group-hover:[animation-play-state:running]">
+                <img
+                  src=""
+                  alt=""
+                  className="aspect-video bg-contrast-200 ring-0 ring-inset ring-background transition-all group-hover:ring-[12px]"
+                />
+              </div>
+
+              <div className="mb-2 flex items-center gap-x-2">
+                <span className="mb-0.5 text-xl font-bold">{vibe.name}</span>
+                <span className="rounded-full border border-foreground px-2 py-0.5 text-xs font-bold text-foreground">
+                  Ecommerce
+                </span>
+              </div>
+              <p className="pr-4 font-light text-foreground">
+                A sleek and modern SaaS website theme inspired by Linear, featuring minimalistic
+                design, intuitive navigation, and a focus on user experience.
+              </p>
+            </Link>
+          ))}
+          {navigation.vibes.map(vibe => (
+            <Link
+              key={vibe.slug}
+              href={`/docs/${vibe.slug}`}
+              className="group ring-primary ring-offset-8 focus:outline-none focus-visible:ring-2"
+            >
+              <div className="marching-ants mb-4 border border-dashed border-transparent [animation-play-state:paused] group-hover:[animation-play-state:running]">
+                <img
+                  src=""
+                  alt=""
+                  className="aspect-video bg-contrast-200 ring-0 ring-inset ring-background transition-all group-hover:ring-[12px]"
+                />
+              </div>
+
+              <div className="mb-2 flex items-center gap-x-2 text-xl font-bold">
+                <span className="mb-0.5">{vibe.name}</span>
+                <span className="rounded-full border border-foreground px-2 py-0.5 text-xs font-bold text-foreground">
+                  Ecommerce
+                </span>
+              </div>
+              <p className="pr-4 font-light text-foreground">
                 A sleek and modern SaaS website theme inspired by Linear, featuring minimalistic
                 design, intuitive navigation, and a focus on user experience.
               </p>
