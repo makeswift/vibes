61f1d41b7c6816285913df84afc5866891085496
Frederico Pena Santiago <frede.stg@gmail.com>
2024-07-26 13:53:18 -0300

    fix: Delete datepicker docs from navigation

--- apps/web/vibes/2px/navigation.ts
+++ apps/web/vibes/2px/navigation.ts
@@ -5,14 +5,13 @@ export const navigation = [
     title: 'Components',
 
     pages: [
-      { title: 'Calendar', slug: 'calendar', file: 'docs/calendar.mdx' },
       {
         title: 'Button',
         slug: 'button',
         file: 'docs/button.mdx',
         component: 'button',
       },
-      { title: 'Datepicker', slug: 'datepicker', file: 'docs/datepicker.mdx' },
+      { title: 'Calendar', slug: 'calendar', file: 'docs/calendar.mdx' },
       { title: 'Rating', slug: 'rating', file: 'docs/rating.mdx' },
     ],
   },
