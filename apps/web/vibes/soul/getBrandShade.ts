c93a23189d33b3e6b169fa74f17a8918763d204a
Joseph Cortez <joseph@monogram.io>
2024-07-27 15:22:39 -0500

    brand shade calculation improvements

--- apps/web/vibes/soul/getBrandShade.ts
+++ apps/web/vibes/soul/getBrandShade.ts
@@ -6,14 +6,15 @@ const colorMap: { [key: number]: number } = {
   300: 0.54,
   400: 0.36,
   500: 0.18,
-  600: -0.41,
-  700: -0.59,
-  800: -0.72,
-  900: -0.85,
+  600: -0.36,
+  700: -0.54,
+  800: -0.67,
+  900: -0.8,
 }
 
 const hslToString = (h: number, s: number, l: number) => `hsl(${h}, ${s}%, ${l}%)`
 const calcLightness = (l: number, mix: number) => (mix >= 0 ? l + (100 - l) * mix : l * (1 + mix))
+const calcSaturation = (s: number, mix: number) => (mix < 0 ? s * (1 + mix) : s)
 
 const getBrandShade = (
   brandName: string | undefined,
@@ -28,7 +29,7 @@ const getBrandShade = (
 
   const [_, h, s, l] = hslMatch
 
-  return hslToString(+h, +s, calcLightness(+l, colorMap[value]))
+  return hslToString(+h, calcSaturation(+s, colorMap[value]), calcLightness(+l, colorMap[value]))
 }
 
 export default getBrandShade
