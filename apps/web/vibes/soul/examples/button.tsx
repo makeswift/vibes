82b4d76d11a991a168ca7ad6c88975686945ba08
Joseph Cortez <joseph@monogram.io>
2024-07-30 12:35:24 -0500

    examples

--- apps/web/vibes/soul/examples/button.tsx
+++ apps/web/vibes/soul/examples/button.tsx
@@ -2,8 +2,14 @@ import Button from '@/vibes/soul/components/button'
 
 export default function Preview() {
   return (
-    <div className="flex min-h-48 items-center justify-center bg-contrast-100 p-5 sm:min-h-64 sm:p-8 lg:min-h-80 lg:p-12">
+    <div className="flex min-h-48 flex-col items-center justify-center gap-10 bg-contrast-100 p-5 sm:min-h-64 sm:p-8 lg:min-h-80 lg:p-20">
       <Button variant="primary" size="default">
+        Shop Now
+      </Button>
+      <Button variant="secondary" size="default">
+        Shop Now
+      </Button>
+      <Button variant="tertiary" size="default">
         Shop Now
       </Button>
     </div>
