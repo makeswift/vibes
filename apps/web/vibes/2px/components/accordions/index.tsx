cc4e5d74dea513fe349b9e65c3c94f46b5c97873
Frederico Pena Santiago <frede.stg@gmail.com>
2024-07-31 15:28:16 -0300

    fix: Accordion padding for small screens

--- apps/web/vibes/2px/components/accordions/index.tsx
+++ apps/web/vibes/2px/components/accordions/index.tsx
@@ -5,7 +5,7 @@ import clsx from 'clsx'
 
 import { ChevronDownIcon } from '../icons'
 
-type AccordionItem = {
+export type AccordionItem = {
   title: ReactNode
   body: ReactNode
 }
@@ -21,31 +21,30 @@ const Accordions = ({ className, accordions, type = 'multiple' }: Props) => {
     <Accordion.Root type={type} className={clsx(className, 'font-body @container')} asChild>
       <ul className="relative w-full ">
         {accordions.map((accordion, i) => (
-          <Accordion.Item key={i} value={`${i + 1}`} asChild>
-            <li className="group relative px-2 pt-5 text-sm font-medium text-foreground @lg:pt-10 @lg:text-lg">
-              <Accordion.Header>
-                <Accordion.Trigger asChild>
-                  <div className="flex w-full cursor-pointer items-center justify-between @lg:pr-5">
-                    <div className="flex-grow text-left font-body ">{accordion.title}</div>
-
-                    <button className="shrink-0">
-                      <ChevronDownIcon
-                        className="text-foreground group-data-[state=open]:rotate-180"
-                        height={16}
-                        width={16}
-                      />
-                    </button>
-                  </div>
-                </Accordion.Trigger>
-              </Accordion.Header>
-
-              <Accordion.Content className="mt-5 w-full overflow-hidden @lg:pr-5">
-                <div className="">{accordion.body}</div>
-              </Accordion.Content>
-
-              <div className="mb-5 mt-5 h-[2px] w-full bg-foreground @lg:mt-10" />
-            </li>
-          </Accordion.Item>
+          <div key={i}>
+            <Accordion.Item value={`${i + 1}`} asChild>
+              <li className="group relative pl-2 pr-2 pt-5 text-sm font-medium text-foreground @lg:pl-0 @lg:pt-10 @lg:text-lg">
+                <Accordion.Header>
+                  <Accordion.Trigger asChild>
+                    <div className="flex w-full cursor-pointer items-center justify-between @lg:pr-5">
+                      <div className="flex-grow text-left font-body ">{accordion.title}</div>
+                      <button className="shrink-0">
+                        <ChevronDownIcon
+                          className="text-foreground group-data-[state=open]:rotate-180"
+                          height={16}
+                          width={16}
+                        />
+                      </button>
+                    </div>
+                  </Accordion.Trigger>
+                </Accordion.Header>
+                <Accordion.Content className="mt-5 w-full overflow-hidden @lg:pr-5">
+                  <div className="">{accordion.body}</div>
+                </Accordion.Content>
+              </li>
+            </Accordion.Item>
+            <div className="mb-5 mt-5 h-[2px] w-full bg-foreground @lg:mt-10" />
+          </div>
         ))}
       </ul>
     </Accordion.Root>
