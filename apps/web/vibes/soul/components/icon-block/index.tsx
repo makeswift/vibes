a3c9778760662fd68d6b70313e940f4af34ed1f8
Joseph Cortez <joseph@monogram.io>
2024-08-01 14:59:16 -0500

    icon block borders and container sizing

--- apps/web/vibes/soul/components/icon-block/index.tsx
+++ apps/web/vibes/soul/components/icon-block/index.tsx
@@ -14,14 +14,15 @@ export const IconBlock = function IconBlock({ list }: IconBlock) {
   return (
     <section className="bg-background text-foreground @container">
       {list?.length && (
-        <ul className="flex flex-wrap justify-center divide-y px-3 @2xl:px-20">
+        <ul className="mx-auto flex w-full max-w-7xl flex-wrap justify-center divide-y divide-contrast-100 px-3 @2xl:px-20">
           {list.map(({ title, description, icon }, idx) => {
             return (
               <li
                 key={idx}
                 className={clsx(
                   'flex flex-col items-center gap-2 px-1 py-10',
-                  list.length !== 4 ? 'w-full @md:w-1/2 @xl:w-1/3' : 'w-1/4'
+                  list.length !== 4 ? 'w-full @md:w-1/2 @xl:w-1/3' : 'w-1/4',
+                  { 'border-t border-contrast-100': idx === 0 }
                 )}
               >
                 {/* Icon */}
