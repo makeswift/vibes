d671580082e4f15e42f9b63be1af874f3707bd90
Mohammed Elhadi Baci <elhadi98@gmail.com>
2024-08-02 12:06:04 +0100

    chore: 2px file uploader component improvements

--- apps/web/vibes/2px/components/file-uploader/index.tsx
+++ apps/web/vibes/2px/components/file-uploader/index.tsx
@@ -1,56 +1,92 @@
+'use client'
+
+import { useRef, useState } from 'react'
+
 import { cn } from '@/lib/utils'
 
 import { CheckIcon, CrossIcon } from '../icons'
 
-interface Props {
-  className?: string
-  limitMessage?: string
-  file?: File
-  setFile: (file: File | undefined) => void
-  variant: 'default' | 'success' | 'error'
+interface Props extends Omit<React.HTMLProps<HTMLInputElement>, 'type' | 'ref'> {
+  variant?: 'default' | 'success' | 'error'
   errorMessage?: string
-  disabled?: boolean
 }
 
 export default function FileUploader({
   className,
-  limitMessage,
-  file = undefined,
-  setFile,
+  label,
   variant = 'default',
   errorMessage,
-  disabled = false,
+  onChange,
+  placeholder,
+  ...props
 }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<
+    | Array<{
+        name: string
+        size: number
+        type: string
+      }>
+    | undefined
+  >()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(e)
+    }
+
+    setFiles(
+      e.target.files?.length
+        ? Array.from(e.target.files).map(file => ({
+            name: file.name,
+            size: file.size,
+            type: file.type,
+          }))
+        : undefined
+    )
+  }
+
+  const handleRemove: React.MouseEventHandler<HTMLButtonElement> = e => {
+    e.stopPropagation()
+
+    setFiles(undefined)
+    if (inputRef.current) {
+      inputRef.current.value = ''
+      inputRef.current.dispatchEvent(new Event('change', { bubbles: true }))
+    }
+  }
+
   return (
     <div className="flex flex-col items-start gap-2 font-body text-xs font-medium">
-      <span>{limitMessage}</span>
+      {label ? (
+        <span className={cn('text-foreground', { 'text-contrast-300': props.disabled })}>
+          {label}
+        </span>
+      ) : null}
       <label
-        htmlFor="file-input"
+        htmlFor={props.id}
         className={cn(
           className,
-          'relative flex h-[3.75rem] w-[25rem] items-center border-[2px] p-3 hover:border-dashed focus:ring-[2px] focus:ring-accent',
+          'relative flex h-[3.75rem] w-[25rem] items-center border-2 p-3 focus-within:ring-2 focus-within:ring-accent',
           {
-            'border-foreground text-foreground': !disabled && variant === 'default',
-            'border-contrast-300 text-contrast-300': disabled,
+            'border-foreground text-foreground hover:border-dashed': !props.disabled,
+            'border-contrast-300 text-contrast-300': props.disabled,
             'border-error': variant === 'error',
             'border-success': variant === 'success',
           }
         )}
       >
-        <input
-          type="file"
-          id="file-input"
-          className="hidden"
-          onChange={e => setFile(e.target.files?.[0])}
-        />
-        <div className="flex w-fit items-center justify-center gap-3">
-          {file ? <span>{file.name}</span> : <span>Choose file</span>}
-          {file && (
-            <button onClick={() => setFile(undefined)}>
+        <input type="file" className="hidden" onChange={handleChange} ref={inputRef} {...props} />
+        {!files ? (
+          <span>{placeholder || 'Choose file'}</span>
+        ) : (
+          <div className="flex w-fit items-center justify-center gap-3">
+            <span>{files.map(file => file.name).join(', ')}</span>
+            <button onClick={handleRemove} type="button">
               <CrossIcon className="h-4 w-4" />
             </button>
-          )}
-        </div>
+          </div>
+        )}
         {variant === 'error' && <CrossIcon className="absolute right-3 h-4 w-4 text-error" />}
         {variant === 'success' && <CheckIcon className="absolute right-3 h-4 w-4 text-error" />}
       </label>
