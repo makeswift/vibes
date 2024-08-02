c27fa6f849e1609783b31be1907b4089ebd4ba49
Mohammed Elhadi Baci <elhadi98@gmail.com>
2024-08-02 10:48:48 +0100

    chore: Refactor 2px Input component to use native input props

--- apps/web/vibes/2px/components/input/index.tsx
+++ apps/web/vibes/2px/components/input/index.tsx
@@ -5,27 +5,12 @@ import React from 'react'
 import { cn } from '@/lib/utils'
 import { CheckIcon, CrossIcon } from '@/vibes/2px/components/icons'
 
-interface Props {
-  className?: string
-  required: boolean
-  value: string
-  setValue: React.Dispatch<React.SetStateAction<string>>
-  variant: 'default' | 'success' | 'error'
+interface Props extends Omit<React.HTMLProps<HTMLInputElement>, 'type'> {
+  variant?: 'default' | 'success' | 'error'
   errorMessage?: string
-  disabled?: boolean
-  placeholder?: string
 }
 
-export default function Input({
-  className,
-  required,
-  value,
-  setValue,
-  variant = 'default',
-  errorMessage,
-  disabled = false,
-  placeholder,
-}: Props) {
+export default function Input({ className, variant = 'default', errorMessage, ...props }: Props) {
   return (
     <div className="font-body text-sm font-medium leading-6">
       <div className="relative flex items-center justify-between">
@@ -33,19 +18,15 @@ export default function Input({
           type="text"
           className={cn(
             className,
-            'custom-caret  h-[3.75rem] w-[25rem] border-2 bg-background px-4 py-2 placeholder-foreground outline-none focus:border-l-transparent focus:border-r-transparent focus:border-t-transparent focus:placeholder-transparent',
+            'custom-caret h-[3.75rem] w-[25rem] border-2 border-foreground bg-background px-4 py-2 text-foreground placeholder-foreground outline-none focus:border-solid focus:border-l-transparent focus:border-r-transparent focus:border-t-transparent focus:placeholder-transparent',
             {
-              'border-contrast-300 text-contrast-300 placeholder-contrast-300': disabled,
-              'border-foreground text-foreground hover:border-dashed ':
-                variant === 'default' && !disabled,
-              'border-foreground  text-error !placeholder-error': variant === 'error',
+              'border-contrast-300 text-contrast-300 placeholder-contrast-300': props.disabled,
+              'hover:border-dashed': !props.disabled,
+              'text-error placeholder-error': variant === 'error',
               'border-foreground': variant === 'success',
             }
           )}
-          required={required}
-          value={value}
-          onChange={e => setValue(e.target.value)}
-          placeholder={placeholder || 'Placeholder...'}
+          {...props}
         />
         {variant === 'success' && <CheckIcon className="absolute right-4 h-6 w-6 text-success" />}
         {variant === 'error' && <CrossIcon className="absolute right-4 h-6 w-6 text-error" />}
--- apps/web/vibes/2px/examples/input.tsx
+++ apps/web/vibes/2px/examples/input.tsx
@@ -7,14 +7,14 @@ import Input from '@/vibes/2px/components/input'
 export default function Preview() {
   const [value, setValue] = React.useState('')
   return (
-    <div className="flex min-h-48 items-center justify-center bg-white p-5 sm:min-h-64 sm:p-8 lg:min-h-80 lg:p-12">
-      <Input
-        required={false}
-        value={value}
-        setValue={setValue}
-        variant="default"
-        disabled={false}
-      />
+    <div className="flex min-h-48 flex-col items-center justify-center gap-5 bg-white p-5 sm:min-h-64 sm:p-8 lg:min-h-80 lg:p-12">
+      <Input variant="default" placeholder="Start typing..." />
+      <hr />
+      <Input variant="success" placeholder="Success input" />
+      <hr />
+      <Input placeholder="Error input" variant="error" />
+      <hr />
+      <Input disabled={true} placeholder="Disabled input" />
     </div>
   )
 }
