7123e34ec9939bd05e696d48ef8e71e7448c0da0
Mohammed Elhadi Baci <elhadi98@gmail.com>
2024-08-05 12:54:46 +0100

    chore: improve text section styling

--- apps/web/vibes/2px/components/text-section/index.tsx
+++ apps/web/vibes/2px/components/text-section/index.tsx
@@ -5,11 +5,11 @@ interface Props {
 
 export default function TextSection({ heading, text }: Props) {
   return (
-    <section className="flex h-fit flex-col bg-primary p-2 font-medium text-foreground @lg:flex-row">
-      <div className="flex h-[25rem] w-full items-center justify-center text-center font-heading text-4xl leading-[2.25rem] -tracking-[0.04rem]  @lg:h-[37.5rem] @lg:max-w-[90rem] @lg:text-6xl @lg:leading-[4rem] @lg:-tracking-[0.0675]">
+    <section className="flex h-fit w-full flex-col justify-center gap-5 bg-primary p-2 font-medium text-foreground @2xl:flex-row">
+      <div className="flex aspect-square w-full items-center justify-center text-center font-heading text-4xl leading-[2.25rem] -tracking-[0.04rem] @2xl:max-w-[90rem] @2xl:text-6xl @2xl:leading-[4rem] @2xl:-tracking-[0.0675]">
         <h2>{heading}</h2>
       </div>
-      <div className="flex font-body text-lg leading-[1.875rem] -tracking-[0.015] @lg:max-w-[44.5rem] @lg:items-center @lg:text-2xl @lg:leading-[2.25rem] @lg:-tracking-[0.0175rem]">
+      <div className="flex font-body text-lg leading-[1.875rem] -tracking-[0.015] @2xl:max-w-[44.5rem] @2xl:items-center @2xl:text-2xl @2xl:leading-[2.25rem] @2xl:-tracking-[0.0175rem]">
         <p>{text}</p>
       </div>
     </section>
