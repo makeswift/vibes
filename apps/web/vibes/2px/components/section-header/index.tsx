d525a1024b24dba17345f0160416cf4e079b08bb
Mohammed Elhadi Baci <elhadi98@gmail.com>
2024-08-07 10:45:26 +0100

    refactor: Update 2px SectionHeader component props and styles

--- apps/web/vibes/2px/components/section-header/index.tsx
+++ apps/web/vibes/2px/components/section-header/index.tsx
@@ -2,31 +2,28 @@ import { cn } from '@/lib/utils'
 
 interface Props {
   className?: string
-  heading: string
-  tag: string
+  title: string
+  subtitle?: string
+  headline?: string
 }
 
-export default function SectionHeader({ className, heading, tag }: Props) {
+export default function SectionHeader({ className, title, headline, subtitle }: Props) {
   return (
     <section
       className={cn(
         className,
-        'flex w-full flex-col items-center justify-center gap-20 border-b-2 border-foreground bg-background px-[0.625rem] py-[7.31rem] text-center text-foreground @lg:py-[5.5rem]'
+        'flex w-full flex-col items-center justify-center gap-20 border-b-2 border-foreground bg-background px-2.5 py-28 text-center text-foreground @lg:py-20'
       )}
     >
-      <div className="@leading-[1.375rem] flex gap-4 font-mono text-xs uppercase leading-[1.125rem] tracking-[0.0225rem] @lg:text-sm">
-        <span>{tag}</span>
-        <span>{tag}</span>
-        <span>{tag}</span>
-      </div>
+      <p className="font-mono text-xs uppercase leading-[1.125rem] tracking-[0.0225rem] @lg:text-sm @lg:leading-snug">
+        {headline}
+      </p>
       <h2 className="w-full font-heading text-3xl font-medium leading-[2.125rem] -tracking-[0.0375rem] @lg:text-6xl @lg:leading-[4rem] @lg:-tracking-[0.0675rem]">
-        {heading}
+        {title}
       </h2>
-      <div className="@leading-[1.375rem] flex gap-4 font-mono text-xs uppercase leading-[1.125rem] tracking-[0.0225rem] @lg:text-sm">
-        <span>{tag}</span>
-        <span>{tag}</span>
-        <span>{tag}</span>
-      </div>
+      <p className="font-mono text-xs uppercase leading-[1.125rem] tracking-[0.0225rem] @lg:text-sm @lg:leading-snug">
+        {subtitle}
+      </p>
     </section>
   )
 }
