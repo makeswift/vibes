1a31258fe85a05d978bbc92de970424f3fbe1e8a
Samantha Shackelford <sami@monogram.io>
2024-08-08 15:45:11 -0400

    fix link label

--- apps/web/vibes/soul/examples/featured-product-list.tsx
+++ apps/web/vibes/soul/examples/featured-product-list.tsx
@@ -53,7 +53,7 @@ export default function Preview() {
     <FeaturedProductList
       title="Off-Race"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."
-      link={{ badge: 'Shop Now', href: '/new-arrivals' }}
+      link={{ label: 'Shop Now', href: '/new-arrivals' }}
       products={featuredProducts}
     />
   )
