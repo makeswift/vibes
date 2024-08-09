7d5838cbb818d9a38163148b559158c892509203
Samantha Shackelford <sami@monogram.io>
2024-08-09 13:07:59 -0400

    add applied and close button to filter panel

--- apps/web/vibes/soul/components/filter-dialog/index.tsx
+++ apps/web/vibes/soul/components/filter-dialog/index.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
 import clsx from 'clsx'
-import { Sliders } from 'lucide-react'
+import { Sliders, X } from 'lucide-react'
 
 import Accordions from '@/vibes/soul/components/accordions'
 import Button from '@/vibes/soul/components/button'
@@ -66,114 +66,127 @@ export const FilterDialog = function FilterDialog() {
   const [selectedTags, setSelectedTags] = useState([] as string[])
 
   return (
-    <>
-      <Dialog.Root open={filterOpen} onOpenChange={setFilterOpen}>
-        <Dialog.Trigger asChild>
-          <Button variant="dark" size="small" className="w-fit">
-            <span className="hidden @xl:block">Filter</span>
-            <Sliders size={18} />
-          </Button>
-        </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 z-30 bg-foreground/50">
-            <Dialog.Content
-              className={clsx(
-                'fixed right-0 top-0 flex h-full flex-col overflow-y-auto bg-background p-20',
-                'transition-transform duration-300 ease-out',
-                // TODO: Get panel to slide open and closed
-                'data-[state=closed]:translate-x-full data-[state=open]:translate-x-0'
-                // filterOpen ? 'translate-x-0' : 'translate-x-full'
-              )}
-            >
-              <>
-                <VisuallyHidden.Root>
-                  <Dialog.Title className="DialogTitle">Filter</Dialog.Title>
-                </VisuallyHidden.Root>
-                <div>
-                  <h2 className="text-2xl">Filters</h2>
-                  <Accordions
-                    className="mt-10"
-                    defaultValue={['1', '2', '3']}
-                    accordions={[
-                      {
-                        title: 'sizing',
-                        content: (
-                          <div className="flex flex-wrap gap-2">
-                            {sizingFilters?.length &&
-                              sizingFilters.map(({ label, amount }, index) => {
-                                return (
-                                  <Chip
-                                    key={index}
-                                    label={label}
-                                    amount={amount}
-                                    onClick={() =>
-                                      setSelectedTags(
-                                        prev =>
-                                          prev.includes(label)
-                                            ? prev.filter(tag => tag !== label) // Remove the label if it's already in the array
-                                            : [...prev, label] // Add the label if it's not in the array
-                                      )
-                                    }
-                                    selected={selectedTags.includes(label)}
-                                  />
-                                )
-                              })}
-                          </div>
-                        ),
-                      },
-                      {
-                        title: 'color',
-                        content: (
-                          <div className="flex flex-wrap gap-2">
-                            {colorFilters?.length &&
-                              colorFilters.map(({ label, amount }, index) => {
-                                return (
-                                  <Chip
-                                    key={index}
-                                    label={label}
-                                    amount={amount}
-                                    onClick={() =>
-                                      setSelectedTags(
-                                        prev =>
-                                          prev.includes(label)
-                                            ? prev.filter(tag => tag !== label) // Remove the label if it's already in the array
-                                            : [...prev, label] // Add the label if it's not in the array
-                                      )
-                                    }
-                                    selected={selectedTags.includes(label)}
-                                  />
-                                )
-                              })}
-                          </div>
-                        ),
-                      },
-                      {
-                        title: 'pricing',
-                        content: (
-                          <div className="flex w-[48%] gap-2">
-                            <Input variant="price" />
-                            <Input variant="price" />
-                          </div>
-                        ),
-                      },
-                    ]}
-                  />
-
-                  <div className="mt-auto flex justify-center gap-2 pt-10">
-                    <Button variant="dark" onClick={() => setFilterOpen(false)}>
-                      Show 25 Results
-                    </Button>
-                    <Button variant="light" onClick={() => setSelectedTags([])}>
-                      Reset
-                    </Button>
-                  </div>
+    <Dialog.Root open={filterOpen} onOpenChange={setFilterOpen}>
+      <Dialog.Trigger asChild>
+        <Button variant="dark" size="small">
+          <span className="hidden @xl:block">Filter</span>
+          <Sliders size={18} />
+        </Button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="absolute inset-0 z-30 bg-foreground/50 @container">
+          <Dialog.Content
+            className={clsx(
+              'absolute right-0 top-0 flex h-full flex-col overflow-y-auto bg-background p-6 @md:p-20',
+              'transition-transform duration-300 ease-out',
+              // TODO: Get panel to slide open and closed
+              'data-[state=closed]:translate-x-full data-[state=open]:translate-x-0'
+              // filterOpen ? 'translate-x-0' : 'translate-x-full'
+            )}
+          >
+            <>
+              <VisuallyHidden.Root>
+                <Dialog.Title className="DialogTitle">Filter</Dialog.Title>
+              </VisuallyHidden.Root>
+              <div>
+                <div className="flex items-center justify-between">
+                  <h2 className="flex gap-2 text-[20px] font-medium @lg:text-2xl">
+                    Filters
+                    {selectedTags.length > 0 && (
+                      <span className="text-contrast-300">{selectedTags.length} applied</span>
+                    )}
+                  </h2>
+                  <Button
+                    variant="light"
+                    size="small"
+                    className="-mr-2 !px-2"
+                    onClick={() => setFilterOpen(false)}
+                  >
+                    <X size={18} strokeWidth={1.5} />
+                  </Button>
                 </div>
-              </>
-            </Dialog.Content>
-          </Dialog.Overlay>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </>
+                <Accordions
+                  className="mt-10"
+                  defaultValue={['1', '2', '3']}
+                  accordions={[
+                    {
+                      title: 'sizing',
+                      content: (
+                        <div className="flex flex-wrap gap-2">
+                          {sizingFilters?.length &&
+                            sizingFilters.map(({ label, amount }, index) => {
+                              return (
+                                <Chip
+                                  key={index}
+                                  label={label}
+                                  amount={amount}
+                                  onClick={() =>
+                                    setSelectedTags(
+                                      prev =>
+                                        prev.includes(label)
+                                          ? prev.filter(tag => tag !== label) // Remove the label if it's already in the array
+                                          : [...prev, label] // Add the label if it's not in the array
+                                    )
+                                  }
+                                  selected={selectedTags.includes(label)}
+                                />
+                              )
+                            })}
+                        </div>
+                      ),
+                    },
+                    {
+                      title: 'color',
+                      content: (
+                        <div className="flex flex-wrap gap-2">
+                          {colorFilters?.length &&
+                            colorFilters.map(({ label, amount }, index) => {
+                              return (
+                                <Chip
+                                  key={index}
+                                  label={label}
+                                  amount={amount}
+                                  onClick={() =>
+                                    setSelectedTags(
+                                      prev =>
+                                        prev.includes(label)
+                                          ? prev.filter(tag => tag !== label) // Remove the label if it's already in the array
+                                          : [...prev, label] // Add the label if it's not in the array
+                                    )
+                                  }
+                                  selected={selectedTags.includes(label)}
+                                />
+                              )
+                            })}
+                        </div>
+                      ),
+                    },
+                    {
+                      title: 'pricing',
+                      content: (
+                        <div className="flex w-[48%] gap-2">
+                          <Input variant="price" />
+                          <Input variant="price" />
+                        </div>
+                      ),
+                    },
+                  ]}
+                />
+
+                <div className="mt-auto flex justify-center gap-2 pt-10">
+                  <Button variant="dark" onClick={() => setFilterOpen(false)}>
+                    Show 25 Results
+                  </Button>
+                  <Button variant="light" onClick={() => setSelectedTags([])}>
+                    Reset
+                  </Button>
+                </div>
+              </div>
+            </>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
 
