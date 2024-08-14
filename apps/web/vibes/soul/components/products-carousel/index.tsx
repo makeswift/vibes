de68cbecc8239fdda476491fa221a6f55a03df4f
Samantha Shackelford <sami@monogram.io>
2024-08-14 12:15:28 -0400

    remove commented out className prop

--- apps/web/vibes/soul/components/products-carousel/index.tsx
+++ apps/web/vibes/soul/components/products-carousel/index.tsx
@@ -2,14 +2,10 @@ import Carousel from '@/vibes/soul/components/carousel'
 import ProductCard, { Product } from '@/vibes/soul/components/product-card'
 
 interface Props {
-  // className?: string
   products: Product[]
 }
 
-export const ProductsCarousel = function ProductsCarousel({
-  // className,
-  products,
-}: Props) {
+export const ProductsCarousel = function ProductsCarousel({ products }: Props) {
   return (
     <Carousel>
       {products.map(product => (
