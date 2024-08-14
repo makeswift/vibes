899df62433e1f7d6b75d406588e55f0602a3aa4e
Samantha Shackelford <sami@monogram.io>
2024-08-13 23:33:24 -0400

    featured image centering

--- apps/web/vibes/soul/components/featured-image/index.tsx
+++ apps/web/vibes/soul/components/featured-image/index.tsx
@@ -20,7 +20,7 @@ type Props = {
 export const FeaturedImage = function FeaturedImage({ title, description, image, cta }: Props) {
   return (
     <section className="relative h-[100dvh] max-h-[880px] bg-primary-shadow @container">
-      <div className="flex h-full flex-col @3xl:flex-row">
+      <div className="mx-auto flex h-full max-w-screen-2xl flex-col @3xl:flex-row">
         <div className="relative h-full w-full @3xl:w-1/2 @5xl:w-3/5">
           <Image
             src={image.src}
@@ -31,8 +31,7 @@ export const FeaturedImage = function FeaturedImage({ title, description, image,
             className="object-cover"
           />
         </div>
-
-        <div className="z-10 mx-auto flex w-full max-w-7xl flex-col items-start justify-end gap-4 px-3 py-10 text-background @xl:px-6 @3xl:w-1/2 @5xl:w-2/5 @5xl:p-20">
+        <div className="z-10  flex w-full  flex-col items-start justify-end gap-4 px-3 py-10 text-background @xl:px-6 @3xl:w-1/2 @5xl:w-2/5 @5xl:p-20">
           <h1 className="max-w-xl text-[40px] font-medium">{title}</h1>
           <p className="max-w-xl pb-2">{description}</p>
           <Button asChild>
