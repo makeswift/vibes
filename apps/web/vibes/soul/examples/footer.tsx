455a6c5d1d09a905ef097fbb7a7f5c1bb216d937
Samantha Shackelford <sami@monogram.io>
2024-08-14 13:08:56 -0400

    footer updates

--- apps/web/vibes/soul/examples/footer.tsx
+++ apps/web/vibes/soul/examples/footer.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import Image from 'next/image'
+
+import { Facebook, Linkedin, Youtube } from 'lucide-react'
+
 import Footer from '@/vibes/soul/components/footer'
 
 export const footerLinks = [
@@ -32,6 +38,46 @@ export const footerLinks = [
 
 const copyright = `© ${new Date().getFullYear()} SOUL - Powered by BigCommerce`
 
+const socialMediaLinks = [
+  {
+    href: '#',
+    icon: <Facebook size={16} />,
+  },
+  {
+    href: '#',
+    icon: <Linkedin size={16} />,
+  },
+  {
+    href: '#',
+    icon: <Youtube size={16} />,
+  },
+]
+
+const contactInformation = {
+  address: 'info@mywebsite.com',
+  phone: '+(1)408 123 4567',
+}
+
+const paymentIconsArray: React.ReactNode[] = [
+  <Image src="https://rstr.in/monogram/vibes/8hv4difQbxs" alt="Visa" width={35} height={24} />,
+  <Image src="https://rstr.in/monogram/vibes/2si5pZsQe24" alt="Amex" width={35} height={24} />,
+  <Image
+    src="https://rstr.in/monogram/vibes/j5TMUICitrf"
+    alt="Mastercard"
+    width={35}
+    height={24}
+  />,
+  <Image src="https://rstr.in/monogram/vibes/bpLWRFd4Myo" alt="Paypal" width={35} height={24} />,
+  <Image
+    src="https://rstr.in/monogram/vibes/cko6FUZ4dQB"
+    alt="Google Pay"
+    width={35}
+    height={24}
+  />,
+  <Image src="https://rstr.in/monogram/vibes/doCkqTXefki" alt="Apple Pay" width={35} height={24} />,
+  <Image src="https://rstr.in/monogram/vibes/yINUOYdzjlz" alt="Bitcoin" width={35} height={24} />,
+]
+
 export default function Preview() {
   return (
     <Footer
@@ -41,6 +87,9 @@ export default function Preview() {
         altText: 'SOUL Logo',
       }}
       copyright={copyright}
+      paymentIcons={paymentIconsArray}
+      socialMediaLinks={socialMediaLinks}
+      contactInformation={contactInformation}
     />
   )
 }
