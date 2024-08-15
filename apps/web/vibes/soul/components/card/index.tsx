17ee3db224c4596c7f5398d87ea623a3b95d4d6e
Samantha Shackelford <sami@monogram.io>
2024-08-15 15:59:13 -0400

    add textContrast to card

--- apps/web/vibes/soul/components/card/index.tsx
+++ apps/web/vibes/soul/components/card/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { ComponentPropsWithoutRef } from 'react'
 
+import clsx from 'clsx'
 import { ArrowUpRight } from 'lucide-react'
 
 export type CardProps = {
@@ -11,12 +12,14 @@ export type CardProps = {
     altText: string
   }
   href: string
+  textContrast?: 'light' | 'dark'
 }
 
 export const Card = function Card({
   title,
   image,
   href,
+  textContrast = 'dark',
   ...props
 }: CardProps & ComponentPropsWithoutRef<'a'>) {
   return (
@@ -27,7 +30,10 @@ export const Card = function Card({
     >
       <ArrowUpRight
         strokeWidth={1.5}
-        className="absolute right-2.5 top-2.5 z-10 transition-transform duration-300 ease-out group-hover:-translate-y-1.5 group-hover:translate-x-1.5 @4xl:right-5 @4xl:top-5"
+        className={clsx(
+          'absolute right-2.5 top-2.5 z-10 transition-transform duration-300 ease-out group-hover:-translate-y-1.5 group-hover:translate-x-1.5 @4xl:right-5 @4xl:top-5',
+          textContrast === 'light' ? 'text-background' : 'text-foreground'
+        )}
       />
       <div className="relative h-full w-full overflow-hidden rounded-lg @4xl:rounded-xl">
         <Image
@@ -37,7 +43,12 @@ export const Card = function Card({
           className="w-full select-none bg-contrast-100 object-cover transition-transform duration-300 ease-out group-hover:scale-105"
         />
       </div>
-      <span className="line-clamp-1 text-lg font-medium text-foreground @sm:absolute @sm:bottom-5 @sm:left-5">
+      <span
+        className={clsx(
+          'line-clamp-1 text-lg font-medium text-foreground @4xl:absolute @4xl:bottom-5 @4xl:left-5',
+          textContrast === 'light' ? '@4xl:text-background' : '@4xl:text-foreground'
+        )}
+      >
         {title}
       </span>
     </Link>
