df1b979435e4db8173333173c72ccb694f96347c
Samantha Shackelford <sami@monogram.io>
2024-08-16 12:38:41 -0400

    remove unnecessary conditional

--- apps/web/vibes/soul/components/featured-products-list/index.tsx
+++ apps/web/vibes/soul/components/featured-products-list/index.tsx
@@ -27,17 +27,17 @@ export const FeaturedProductsList = function FeaturedProductsList({
     <section className="bg-background @container">
       <div className="relative mx-auto flex max-w-screen-2xl flex-col gap-6 py-10 @4xl:flex-row @4xl:py-24 @5xl:px-20">
         <div
-          className="4xl:w-1/2 w-full top-28 flex items-start justify-between gap-4 self-start px-3 
+          className="4xl:w-1/2 top-28 flex w-full items-start justify-between gap-4 self-start px-3 
             @xl:px-6 @4xl:sticky @4xl:max-w-md @4xl:flex-col @4xl:items-start @4xl:justify-start @5xl:px-0 @6xl:w-4/12
           "
         >
           <div>
             {title && (
-              <h2 className="text-lg font-semibold leading-none text-foreground @4xl:text-6xl @4xl:font-medium font-heading">
+              <h2 className="font-heading text-lg font-semibold leading-none text-foreground @4xl:text-6xl @4xl:font-medium">
                 {title}
               </h2>
             )}
-            {description && <p className="pb-2 text-foreground mt-1.5">{description}</p>}
+            {description && <p className="mt-1.5 pb-2 text-foreground">{description}</p>}
           </div>
           {cta && (
             <Button
@@ -49,7 +49,7 @@ export const FeaturedProductsList = function FeaturedProductsList({
           )}
         </div>
 
-        {products && <ProductList products={products} className="4xl:w-1/2 @6xl:w-8/12" />}
+        <ProductList products={products} className="4xl:w-1/2 @6xl:w-8/12" />
       </div>
     </section>
   )
