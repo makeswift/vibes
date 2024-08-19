41a9cfefcaf78ab239879e58b51cfbc7546f7833
Samantha Shackelford <sami@monogram.io>
2024-08-19 12:24:58 -0400

    replace arbitrary font size

--- apps/web/vibes/soul/components/products-header/index.tsx
+++ apps/web/vibes/soul/components/products-header/index.tsx
@@ -15,7 +15,7 @@ export const ProductsHeader = function ProductsHeader({ title, numberOfProducts 
     <>
       {/* Products Header */}
       <div className="relative z-10 flex items-center justify-between bg-background pb-10 pt-28 text-foreground @container @lg:pt-44">
-        <h1 className="pl-3 text-xl font-medium @xl:pl-6 @2xl:text-[40px] @5xl:pl-20 font-heading">
+        <h1 className="pl-3 font-heading text-xl font-medium leading-none @xl:pl-6 @2xl:text-5xl @5xl:pl-20">
           {title} <span className="text-contrast-200">{numberOfProducts}</span>
         </h1>
         <div className="flex gap-2 pr-3 @xl:pr-6 @5xl:pr-20">
