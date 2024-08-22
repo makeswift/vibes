87aa821c37311a0451b5a4946341c703a6624d2a
Samantha Shackelford <sami@monogram.io>
2024-08-22 14:11:07 -0400

    fix featured products list button padding

--- apps/web/vibes/soul/components/featured-products-list/index.tsx
+++ apps/web/vibes/soul/components/featured-products-list/index.tsx
@@ -27,8 +27,7 @@ export const FeaturedProductsList = function FeaturedProductsList({
       <div className="relative mx-auto flex max-w-screen-2xl flex-col gap-6 py-10 @4xl:flex-row @4xl:py-24 @5xl:px-20">
         <div
           className="4xl:w-1/2 top-28 flex w-full items-start justify-between gap-4 self-start px-3 
-            @xl:px-6 @4xl:sticky @4xl:max-w-md @4xl:flex-col @4xl:items-start @4xl:justify-start @5xl:px-0 @6xl:w-4/12
-          "
+            @xl:px-6 @4xl:sticky @4xl:max-w-md @4xl:flex-col @4xl:items-start @4xl:justify-start @5xl:px-0 @6xl:w-4/12"
         >
           <div>
             {title && (
@@ -39,10 +38,7 @@ export const FeaturedProductsList = function FeaturedProductsList({
             {description && <p className="mt-1.5 max-w-md pb-2 text-foreground">{description}</p>}
           </div>
           {cta && (
-            <Button
-              className="h-5 bg-transparent !px-0 text-sm @4xl:h-12 @4xl:bg-primary @4xl:!px-6"
-              asChild
-            >
+            <Button className="h-5 bg-transparent text-sm @4xl:h-12 @4xl:bg-primary" asChild>
               <Link href={cta.href}>{cta.label}</Link>
             </Button>
           )}
