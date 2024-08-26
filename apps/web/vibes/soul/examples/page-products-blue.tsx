d1a0e98c976fbcc7f95c9a441f0bd42e9fe5dc02
Samantha Shackelford <sami@monogram.io>
2024-08-26 18:54:33 -0400

    import headerLink into products pages

---
+++ apps/web/vibes/soul/examples/page-products-blue.tsx
@@ -0,0 +1,30 @@
+import AnnouncementBar from '@/vibes/soul/components/announcement-bar'
+import Header from '@/vibes/soul/components/header'
+import { headerLinks } from '@/vibes/soul/examples/header-blue'
+
+const locales = [
+  { id: '1', region: 'US', language: 'EN' },
+  { id: '2', region: 'FR', language: 'FR' },
+  { id: '3', region: 'DE', language: 'DC' },
+  { id: '4', region: 'IT', language: 'IT' },
+]
+
+export default function Preview() {
+  return (
+    <>
+      <AnnouncementBar>
+        Get <strong>15% off</strong> and free shipping with discount code{' '}
+        <strong>&quot;welcome&quot;</strong>
+      </AnnouncementBar>
+
+      <Header
+        links={headerLinks}
+        logo="SOUL"
+        cartHref="#"
+        accountHref="#"
+        locales={locales}
+        activeLocale="EN"
+      />
+    </>
+  )
+}
