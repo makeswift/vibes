90242a62e00af55ef1bb1db6d8d8f16a87b1f732
Samantha Shackelford <sami@monogram.io>
2024-09-12 13:49:06 -0400

    add id to accordions

--- apps/web/vibes/soul/examples/accordions.tsx
+++ apps/web/vibes/soul/examples/accordions.tsx
@@ -3,21 +3,25 @@ import { Accordions } from '@/vibes/soul/components/accordions'
 export default function Preview() {
   const accordions = [
     {
+      id: '1',
       title: 'Description',
       content:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
     },
     {
+      id: '2',
       title: 'Description',
       content:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
     },
     {
+      id: '3',
       title: 'Description',
       content:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
     },
     {
+      id: '4',
       title: 'Description',
       content:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
