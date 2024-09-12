374d4959ac795f034f456fbfa67a81c5642deea1
Samantha Shackelford <sami@monogram.io>
2024-09-12 16:59:58 -0400

    add id to product-gallery images

--- apps/web/vibes/soul/components/product-detail/product-gallery.tsx
+++ apps/web/vibes/soul/components/product-detail/product-gallery.tsx
@@ -7,15 +7,16 @@ import { clsx } from 'clsx'
 import useEmblaCarousel from 'embla-carousel-react'
 
 interface Image {
+  id: string
   altText: string
   src: string
 }
 
-export interface ProductGallery {
+export interface ProductGalleryProps {
   images: Image[]
 }
 
-export const ProductGallery = ({ images }: ProductGallery) => {
+export const ProductGallery = ({ images }: ProductGalleryProps) => {
   const [previewImage, setPreviewImage] = useState(0)
   const [emblaRef, emblaApi] = useEmblaCarousel()
 
@@ -41,9 +42,9 @@ export const ProductGallery = ({ images }: ProductGallery) => {
     <div className="relative mt-[60px] flex h-96 w-full items-center overflow-hidden bg-contrast-100 @2xl:h-[550px] @4xl:mt-0 @4xl:h-full">
       <div className="my-auto h-full max-h-[800px] w-full overflow-hidden" ref={emblaRef}>
         <div className="flex h-full w-full">
-          {images.map((image, index) => (
+          {images.map(image => (
             <div
-              key={index}
+              key={image.id}
               className="relative h-full w-full min-w-0 shrink-0 grow-0 basis-full items-center"
             >
               <Image
