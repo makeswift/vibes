80f08f5b10d96d30a4f9ca55f449a25d841d104a
Andrew Reifman <andrewr23@gmail.com>
2024-09-19 15:52:57 -0400

    fix: sitemap setup

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const dev = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://vibes.site/docs',
+  siteUrl: dev ? 'http://localhost:3000' : 'https://vibes.site/',
   generateRobotsTxt: true,
 }
