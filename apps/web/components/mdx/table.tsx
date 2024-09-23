05fbd8ddc2cdb00b64dba0c548f1fe6a34d4de9a
Andrew Reifman <andrewr23@gmail.com>
2024-09-23 15:22:22 -0400

    fix: stripe pattern css

--- apps/web/components/mdx/table.tsx
+++ apps/web/components/mdx/table.tsx
@@ -1,6 +1,6 @@
 export default function Table({ children }: React.HTMLAttributes<HTMLTableElement>) {
   return (
-    <div className="relative mb-10 mt-8 w-full overflow-auto">
+    <div className="relative mb-10 mt-6 w-full overflow-auto">
       <table className="m-0 table w-full text-sm">{children}</table>
     </div>
   )
