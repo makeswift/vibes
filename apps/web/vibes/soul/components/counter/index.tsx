d1902f7e91abf041b314c910c698f906e93ce76b
Samantha Shackelford <sami@monogram.io>
2024-10-14 18:34:46 -0400

    remove counter form logic

--- apps/web/vibes/soul/components/counter/index.tsx
+++ apps/web/vibes/soul/components/counter/index.tsx
@@ -2,23 +2,19 @@ import { DecrementButton } from './decrement-button'
 import { IncrementButton } from './increment-button'
 
 interface Props {
-  id: string
   current?: number
   max?: number
   decrementAriaLabel?: string
   incrementAriaLabel?: string
-  updateLineItemQuantityAction({ id, quantity }: { id: string; quantity: number }): Promise<void>
 }
 
 // TODO: This can be refactored for the PDP page. Use Input instead of span.
 
 export const Counter = function Counter({
-  id,
   current = 0,
   max,
   decrementAriaLabel,
   incrementAriaLabel,
-  updateLineItemQuantityAction,
 }: Props) {
   const decrement = () => {
     return current - 1
@@ -29,15 +25,11 @@ export const Counter = function Counter({
 
   return (
     <div className="flex items-center rounded-lg border">
-      <form action={updateLineItemQuantityAction.bind(null, { id, quantity: decrement() })}>
-        <DecrementButton ariaLabel={decrementAriaLabel} />
-      </form>
+      <DecrementButton ariaLabel={decrementAriaLabel} />
       <span className="flex w-8 select-none justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary ">
         {current}
       </span>
-      <form action={updateLineItemQuantityAction.bind(null, { id, quantity: increment() })}>
-        <IncrementButton ariaLabel={incrementAriaLabel} />
-      </form>
+      <IncrementButton ariaLabel={incrementAriaLabel} />
     </div>
   )
 }
