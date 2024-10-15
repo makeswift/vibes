2c02df35363550f4c2c1b315a4f78a5c2236b4e6
Alan Pledger <apledger3@gmail.com>
2024-10-15 14:30:09 -0400

    refactor: remove compare-products

--- apps/web/vibes/soul/pages/products/index.tsx
+++ apps/web/vibes/soul/pages/products/index.tsx
@@ -1,10 +1,11 @@
 import { AnnouncementBar } from '@/vibes/soul/primitives/announcement-bar'
 import { Breadcrumb } from '@/vibes/soul/primitives/breadcrumbs'
 import { Product } from '@/vibes/soul/primitives/product-card'
-import { CompareProducts } from '@/vibes/soul/sections/compare-products'
 import { FeaturedProductsCarousel } from '@/vibes/soul/sections/featured-products-carousel'
 import { Footer, Section } from '@/vibes/soul/sections/footer'
 import { Header, Links } from '@/vibes/soul/sections/header'
+import { ProductsListSection } from '@/vibes/soul/sections/products-list-section'
+import { Filter } from '@/vibes/soul/sections/products-list-section/filters'
 
 interface Image {
   src: string
@@ -26,6 +27,49 @@ const locales = [
   { id: '2', region: 'FR', language: 'FR' },
   { id: '3', region: 'DE', language: 'DC' },
   { id: '4', region: 'IT', language: 'IT' },
+]
+
+const filters: Filter[] = [
+  {
+    name: 'color',
+    label: 'Color',
+    type: 'checkbox-group',
+    options: [
+      { label: 'Red', value: 'red' },
+      { label: 'Green', value: 'green' },
+      { label: 'Blue', value: 'blue' },
+    ],
+  },
+  {
+    name: 'size',
+    label: 'Size',
+    type: 'checkbox-group',
+    options: [
+      { label: 'Small', value: 'sm' },
+      { label: 'Medium', value: 'md' },
+      { label: 'Large', value: 'lg' },
+    ],
+  },
+  {
+    name: 'price',
+    label: 'Price',
+    type: 'range',
+    min: 0,
+    max: 200,
+    minLabel: '$',
+    maxLabel: '$',
+  },
+  {
+    name: 'rating',
+    label: 'Rating',
+    type: 'rating',
+  },
+]
+
+const sortOptions = [
+  { label: 'Price: Low to High', value: 'price-asc' },
+  { label: 'Price: High to Low', value: 'price-desc' },
+  { label: 'Newest', value: 'newest' },
 ]
 
 export const ProductsPage = function ProductsPage({
@@ -51,7 +95,14 @@ export const ProductsPage = function ProductsPage({
         activeLocale="EN"
         locales={locales}
       />
-      <CompareProducts breadcrumbs={breadcrumbs} title={title} products={products} pages={5} />
+      <ProductsListSection
+        breadcrumbs={breadcrumbs}
+        title={title}
+        products={products}
+        totalCount={products.length}
+        sortOptions={sortOptions}
+        filters={filters}
+      />
       <FeaturedProductsCarousel title="Recently Viewed" products={products} />
       <Footer sections={footerLinks} logo={logo} copyright={copyright} />
     </>
