3d53f2b7b814468f83ed435c270f7ec837fdd5bc
Alan Pledger <apledger3@gmail.com>
2024-10-15 15:48:38 -0400

    feat: add label and error

--- apps/web/vibes/soul/form/toggle-group/index.tsx
+++ apps/web/vibes/soul/form/toggle-group/index.tsx
@@ -3,6 +3,10 @@
 import * as React from 'react'
 
 import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group'
+import clsx from 'clsx'
+
+import { ErrorMessage } from '@/vibes/soul/form/error-message'
+import { Label } from '@/vibes/soul/form/label'
 
 interface Option {
   value: string
@@ -15,20 +19,27 @@ export const ToggleGroup = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> & {
     label?: string
     options: Option[]
+    error?: string
   }
->(({ label, options, ...rest }, ref) => {
+>(({ id, label, options, error, className, ...rest }, ref) => {
   return (
-    <ToggleGroupPrimitive.Root {...rest} ref={ref} aria-label={label} className="flex gap-2">
-      {options.map(option => (
-        <ToggleGroupPrimitive.Item
-          value={option.value}
-          aria-label={option.label}
-          className="whitespace-nowrap rounded-full border border-transparent px-3 py-2 font-body text-sm font-normal ring-primary transition-colors focus-visible:outline-0 focus-visible:ring-2 data-[state=off]:bg-contrast-100 data-[state=on]:bg-foreground data-[state=on]:text-background data-[state=off]:hover:border-contrast-300"
-        >
-          {option.label}
-        </ToggleGroupPrimitive.Item>
-      ))}
-    </ToggleGroupPrimitive.Root>
+    <div className={clsx('space-y-2', className)}>
+      {label !== undefined && label !== '' && <Label htmlFor={id}>{label}</Label>}
+      <ToggleGroupPrimitive.Root {...rest} ref={ref} aria-label={label} className="flex gap-2">
+        {options.map(option => (
+          <ToggleGroupPrimitive.Item
+            key={option.value}
+            value={option.value}
+            aria-label={option.label}
+            disabled={option.disabled}
+            className="whitespace-nowrap rounded-full border border-transparent px-3 py-2 font-body text-sm font-normal ring-primary transition-colors focus-visible:outline-0 focus-visible:ring-2 data-[disabled]:cursor-not-allowed data-[state=off]:bg-contrast-100 data-[state=on]:bg-foreground data-[state=on]:text-background data-[disabled]:opacity-50 data-[disabled]:hover:border-transparent data-[state=off]:hover:border-contrast-300"
+          >
+            {option.label}
+          </ToggleGroupPrimitive.Item>
+        ))}
+      </ToggleGroupPrimitive.Root>
+      {error !== undefined && error !== '' && <ErrorMessage>{error}</ErrorMessage>}
+    </div>
   )
 })
 
