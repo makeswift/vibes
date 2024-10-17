5f36d507f12666f2cd6a16992439f20aedc85ae7
Andrew Reifman <andrewr23@gmail.com>
2024-10-17 15:06:06 -0400

    fix: remove shadcn logic

--- apps/web/vibes/soul/examples/primitives/button/index.tsx
+++ apps/web/vibes/soul/examples/primitives/button/index.tsx
@@ -5,9 +5,7 @@ import { Button } from '@/vibes/soul/primitives/button'
 export default function Preview() {
   return (
     <div className="mx-auto mt-10 grid max-w-sm grid-cols-2 justify-items-center gap-y-6">
-      <Button variant="primary" asChild>
-        <Link href="/shop">Shop Now</Link>
-      </Button>
+      <Button variant="primary">Shop Now</Button>
       <Button variant="primary" loading>
         Shop Now
       </Button>
--- apps/web/vibes/soul/primitives/button/index.tsx
+++ apps/web/vibes/soul/primitives/button/index.tsx
@@ -1,4 +1,3 @@
-import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
 import { Loader2 } from 'lucide-react'
 
@@ -7,7 +6,6 @@ export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'large' | 'medium' | 'small' | 'icon'
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
   loading?: boolean
-  asChild?: boolean
   type?: 'button' | 'submit' | 'reset'
 }
 
@@ -19,13 +17,11 @@ export const Button = function Button({
   disabled,
   className,
   children,
-  asChild,
   type = 'button',
   ...props
 }: Props) {
-  const Comp = asChild ? Slot : 'button'
   return (
-    <Comp
+    <button
       className={clsx(
         'relative z-0 select-none justify-center overflow-hidden rounded-full border text-center font-medium leading-normal transition-colors focus-visible:outline-none focus-visible:ring-2',
         {
@@ -34,7 +30,7 @@ export const Button = function Button({
           tertiary: 'border-contrast-200 bg-background text-background ring-primary',
         }[variant],
         // After Pseudo Element / Animated Background Styles
-        'after:absolute after:inset-0 after:-z-10 after:-translate-x-full after:rounded-full after:transition-[opacity,transform] after:duration-300 after:[animation-timing-function:cubic-bezier(0,0.25,0,1)]',
+        'after:absolute after:inset-0 after:-z-10 after:-translate-x-[105%] after:rounded-full after:transition-[opacity,transform] after:duration-300 after:[animation-timing-function:cubic-bezier(0,0.25,0,1)]',
         !loading && !disabled && 'hover:after:translate-x-0',
         {
           primary: 'after:bg-background/40',
@@ -52,8 +48,8 @@ export const Button = function Button({
       <>
         <span
           className={clsx(
-            'flex h-full items-center justify-center transition-transform duration-300 ease-in-out',
-            loading ? '-translate-y-full' : 'translate-y-0',
+            'flex items-center justify-center transition-all duration-300 ease-in-out',
+            loading ? '-translate-y-10 opacity-0' : 'translate-y-0 opacity-100',
             {
               icon: 'p-2.5 text-sm',
               small: 'gap-x-2 px-4 py-2.5 text-sm',
@@ -68,13 +64,13 @@ export const Button = function Button({
 
         <span
           className={clsx(
-            'absolute inset-0 grid place-content-center transition-transform duration-300 ease-in-out',
-            loading ? 'translate-y-0' : 'translate-y-full'
+            'absolute inset-0 grid place-content-center transition-all duration-300 ease-in-out',
+            loading ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'
           )}
         >
           <Loader2 className={clsx('animate-spin', variant === 'tertiary' && 'text-foreground')} />
         </span>
       </>
-    </Comp>
+    </button>
   )
 }
