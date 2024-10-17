8fffbc3b31148789df05f8aaa9f71f30d6354156
Samantha Shackelford <sami@monogram.io>
2024-10-17 11:29:30 -0400

    chore: separate out soul vibe

--- apps/web/vibes/2px/examples/marquee-section.tsx
+++
@@ -1,9 +0,0 @@
-import MarqueeSection from '@/vibes/2px/components/marquee-section'
-
-export default function Preview() {
-  return (
-    <div className="flex min-h-48 max-w-[90rem] items-center justify-center bg-white py-5 sm:min-h-64 sm:py-8 lg:min-h-80 lg:py-12">
-      <MarqueeSection text={`Summer 24’ we moved into Reform´s bright showroom in Copenhagen.`} />
-    </div>
-  )
-}
