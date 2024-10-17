8d59e5910356c5d8c3d9caacb20363b920cca2ba
Andrew Reifman <andrewr23@gmail.com>
2024-10-17 01:02:50 -0400

    fix: buttons styles and color vars

--- apps/web/vibes/soul/examples/primitives/button/index.tsx
+++ apps/web/vibes/soul/examples/primitives/button/index.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 import { Button } from '@/vibes/soul/primitives/button'
 
 export default function Preview() {
   return (
     <div className="mx-auto mt-10 grid max-w-sm grid-cols-2 justify-items-center gap-y-6">
-      <Button variant="primary">Shop Now</Button>
+      <Button variant="primary" asChild>
+        <Link href="/shop">Shop Now</Link>
+      </Button>
       <Button variant="primary" loading>
         Shop Now
       </Button>
