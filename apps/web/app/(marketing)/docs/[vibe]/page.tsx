36d80c6fe44a342b7097c2413342bd2720c7401c
Alan Pledger <apledger3@gmail.com>
2024-10-21 15:45:59 -0400

    fix: page navigation

--- apps/web/app/(marketing)/docs/[vibe]/page.tsx
+++ apps/web/app/(marketing)/docs/[vibe]/page.tsx
@@ -1,19 +1,22 @@
 import { notFound, redirect } from 'next/navigation'
 
-import { navigation } from '@/components/navigation'
+import { getChapter, mapVibesToNavigation } from '@/components/navigation'
+import * as Vibes from '@/vibes'
 
 export async function generateStaticParams() {
-  return navigation.vibes.map(vibe => ({ vibe: vibe.slug }))
+  const navigation = mapVibesToNavigation(Vibes)
+
+  return navigation.chapters.map(vibe => ({ vibe: vibe.slug }))
 }
 
 export default function Page({ params }: { params: { vibe: string } }) {
-  const vibe = navigation.vibes.find(vibe => vibe.slug === params.vibe)
+  const chapter = getChapter(Vibes, params.vibe)
 
-  if (!vibe) return notFound()
+  if (!chapter) return notFound()
 
-  const page = vibe.groups[0].pages[0]
+  const page = chapter.groups[0].pages[0]
 
   if (!page) return notFound()
 
-  return redirect(`/docs/${vibe.slug}/${page.slug}`)
+  return redirect(`/docs/${chapter.slug}/${page.slug}`)
 }
