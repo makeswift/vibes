155b3ca425560abbfd4a651ec88db52f51d1df3c
Andrew Reifman <andrewr23@gmail.com>
2024-10-22 14:31:57 -0400

    search results, animated link styles

--- apps/web/vibes/soul/primitives/animated-link/index.tsx
+++ apps/web/vibes/soul/primitives/animated-link/index.tsx
@@ -1,8 +1,7 @@
 import Link from 'next/link'
+import { CSSProperties } from 'react'
 
 import { clsx } from 'clsx'
-
-import './style.css'
 
 export interface AnimatedLinkProps {
   href: string
@@ -12,8 +11,15 @@ export interface AnimatedLinkProps {
 
 export const AnimatedLink = function AnimatedLink({ href, label, className }: AnimatedLinkProps) {
   return (
-    <Link href={href} className={clsx('animated-link animated-underline', className)}>
-      <span>{label}</span>
+    <Link
+      href={href}
+      className={clsx(
+        'origin-left pb-0.5 transition-[background-size] duration-500 [background:linear-gradient(0deg,hsl(var(--primary)),hsl(var(--primary)))_no-repeat_left_bottom_/_0_var(--bg-h)] hover:bg-[size:100%_var(--bg-h)] focus-visible:bg-[size:100%_var(--bg-h)]',
+        className
+      )}
+      style={{ '--bg-h': '2px' } as CSSProperties}
+    >
+      {label}
     </Link>
   )
 }
