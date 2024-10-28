7fdc5982a9e5dbb6ee5dd3e3fb53e159f1519a50
Jorge Moya <jorge.moya@bigcommerce.com>
2024-10-28 15:10:39 -0500

    feat: adds  form input

--- apps/web/vibes/soul/form/input/index.tsx
+++ apps/web/vibes/soul/form/input/index.tsx
@@ -39,7 +39,7 @@ export const Input = React.forwardRef<
           ref={ref}
           className={clsx(
             'placeholder-contrast-gray-500 w-full bg-transparent px-6 py-3 text-sm text-foreground [appearance:textfield] placeholder:font-normal focus:outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
-            { 'py-2.5 pl-8 pr-4': prepend }
+            { 'py-2.5 pe-4 ps-12': prepend }
           )}
         />
       </div>
