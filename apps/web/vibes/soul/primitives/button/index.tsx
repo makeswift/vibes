cba0acabd805738b37abf428e341aa668872790f
Andrew Reifman <andrewr23@gmail.com>
2024-10-28 18:25:58 -0400

    fix: button height

--- apps/web/vibes/soul/primitives/button/index.tsx
+++ apps/web/vibes/soul/primitives/button/index.tsx
@@ -25,7 +25,7 @@ export const Button = function Button({
   return (
     <button
       className={clsx(
-        'relative z-0 inline-block select-none justify-center overflow-hidden rounded-full border text-center font-medium leading-normal transition-all duration-300 focus-visible:outline-none focus-visible:ring-2',
+        'relative z-0 h-fit overflow-hidden rounded-full border font-medium leading-normal transition-all duration-300 focus-visible:outline-none focus-visible:ring-2',
         {
           primary: 'border-primary bg-primary text-foreground ring-foreground',
           secondary: 'border-foreground bg-foreground text-background ring-primary',
@@ -53,10 +53,10 @@ export const Button = function Button({
             'inline-flex items-center justify-center transition-all duration-300 ease-in-out',
             loading ? '-translate-y-10 opacity-0' : 'translate-y-0 opacity-100',
             {
-              icon: 'p-2.5 text-sm',
-              small: 'gap-x-2 px-4 py-2.5 text-sm',
-              medium: 'gap-x-2.5 px-5 py-3 text-base',
-              large: 'gap-x-3 px-6 py-4 text-base',
+              icon: 'min-h-10 p-2.5 text-sm',
+              small: 'min-h-10 gap-x-2 px-4 py-2.5 text-sm',
+              medium: 'min-h-12 gap-x-2.5 px-5 py-3 text-base',
+              large: 'min-h-14 gap-x-3 px-6 py-4 text-base',
             }[size],
             variant === 'secondary' && 'mix-blend-difference'
           )}
