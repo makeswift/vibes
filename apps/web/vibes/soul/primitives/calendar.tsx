fc1f189402d983db9e17fdc4d9fd95e59776597e
Jorge Moya <jorge.moya@bigcommerce.com>
2024-10-28 15:04:02 -0500

    fix: more spacing fixes

--- apps/web/vibes/soul/primitives/calendar.tsx
+++ apps/web/vibes/soul/primitives/calendar.tsx
@@ -12,23 +12,25 @@ export function Calendar({
   return (
     <DayPicker
       className={clsx(
-        'w-[304px] rounded-lg border border-contrast-100 bg-background p-3',
+        'box-content w-[280px] rounded-lg border border-contrast-100 bg-background p-3',
         className
       )}
       classNames={{
         months: 'relative',
-        month_caption: 'flex justify-center w-full font-medium pb-px',
+        month_caption: 'flex justify-center w-full font-medium pb-0.5',
         nav: 'absolute flex justify-between w-full',
         button_next:
           'rotate-180 rounded-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-foreground',
         button_previous:
           'rounded-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-foreground',
-        weekdays: 'flex py-px',
+        month_grid: 'flex flex-col gap-0.5',
+        weeks: 'flex flex-col gap-0.5',
+        weekdays: 'flex',
         weekday: 'flex h-10 w-10 items-center justify-center text-xs font-medium',
-        week: 'flex py-px',
+        week: 'flex',
         day: 'h-10 w-10 flex text-xs font-medium group p-0',
         day_button:
-          'h-full w-full flex items-center justify-center rounded-full group-data-[selected=true]:bg-primary group-data-[selected=true]/middle:bg-primary-highlight hover:border hover:border-contrast-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-foreground disabled:hover:border-none',
+          'h-full w-full flex items-center justify-center rounded-full group-data-[selected=true]:bg-primary group-data-[selected=true]/middle:bg-transparent hover:border hover:border-contrast-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-foreground disabled:hover:border-none',
         disabled: 'text-contrast-300',
         outside: 'text-contrast-300',
         range_start: 'bg-gradient-to-l from-primary-highlight',
