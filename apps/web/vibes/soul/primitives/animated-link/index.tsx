a4986a64860690cc53eee4620d80ba0211dcdb99
Andrew Reifman <andrewr23@gmail.com>
2024-10-30 14:07:06 -0400

    fix: remove css variable from animated link

--- apps/web/vibes/soul/primitives/animated-link/index.tsx
+++ apps/web/vibes/soul/primitives/animated-link/index.tsx
@@ -14,10 +14,9 @@ export const AnimatedLink = function AnimatedLink({ href, label, className }: An
     <Link
       href={href}
       className={clsx(
-        'origin-left pb-0.5 font-semibold transition-[background-size] duration-500 [background:linear-gradient(0deg,hsl(var(--primary)),hsl(var(--primary)))_no-repeat_left_bottom_/_0_var(--bg-h)] hover:bg-[size:100%_var(--bg-h)] focus:outline-none focus-visible:bg-[size:100%_var(--bg-h)]',
+        'origin-left pb-0.5 font-semibold transition-[background-size] duration-500 [background:linear-gradient(0deg,hsl(var(--primary)),hsl(var(--primary)))_no-repeat_left_bottom_/_0_2px] hover:bg-[size:100%_2px] focus:outline-none focus-visible:bg-[size:100%_2px]',
         className
       )}
-      style={{ '--bg-h': '2px' } as CSSProperties}
     >
       {label}
     </Link>
