fd572f598265bd65fbffe8aa378886fce09b36b5
Alan Pledger <apledger3@gmail.com>
2024-10-31 14:44:10 -0400

    feat: add order-list components

--- apps/web/vibes/soul/examples/sections/order-list-section/electric.tsx
+++ apps/web/vibes/soul/examples/sections/order-list-section/electric.tsx
@@ -1,5 +1,75 @@
+import { getProducts } from '@/vibes/soul/data'
 import { OrderListSection } from '@/vibes/soul/sections/order-list-section'
 
+const products = getProducts('Electric')
+
+const orders = [
+  {
+    id: '1',
+    totalPrice: '$100',
+    status: 'Delivered',
+    products: products
+      .filter(() => Math.random() > 0.5)
+      .map(({ id, title, subtitle, image, href }) => ({
+        id,
+        title,
+        subtitle,
+        image,
+        href,
+        price: `${Math.floor(Math.random() * 500)}`,
+      })),
+  },
+  {
+    id: '2',
+    totalPrice: '$150',
+    status: 'Delivered',
+    products: products
+      .filter(() => Math.random() > 0.5)
+      .map(({ id, title, subtitle, image, href }) => ({
+        id,
+        title,
+        subtitle,
+        image,
+        href,
+        price: `${Math.floor(Math.random() * 500)}`,
+      })),
+  },
+  {
+    id: '3',
+    totalPrice: '$500',
+    status: 'Delivered',
+    products: products
+      .filter(() => Math.random() > 0.5)
+      .map(({ id, title, subtitle, image, href }) => ({
+        id,
+        title,
+        subtitle,
+        image,
+        href,
+        price: `${Math.floor(Math.random() * 500)}`,
+      })),
+  },
+]
+
+async function orderReturnAction({ id }: { id: string }) {
+  'use server'
+
+  console.log('Returning order', id)
+}
+
+async function orderTrackAction({ id }: { id: string }) {
+  'use server'
+
+  console.log('Tracking order', id)
+}
+
 export default function Preview() {
-  return <OrderListSection />
+  return (
+    <OrderListSection
+      orders={orders}
+      orderReturnAction={orderReturnAction}
+      orderTrackAction={orderTrackAction}
+      paginationInfo={{ startCursor: '1', endCursor: '5' }}
+    />
+  )
 }
--- apps/web/vibes/soul/sections/order-list-section/index.tsx
+++ apps/web/vibes/soul/sections/order-list-section/index.tsx
@@ -1,3 +1,45 @@
-export function OrderListSection() {
-  return <div>Order List Section</div>
+import { CursorPagination, CursorPaginationInfo } from '@/vibes/soul/primitives/cursor-pagination'
+
+import { OrderList } from './order-list'
+
+type OrderProduct = {
+  id: string
+  title: string
+  subtitle?: string
+  price: string
+  href: string
+  image?: { src: string; alt: string }
+}
+
+type Order = {
+  id: string
+  totalPrice: string
+  status: string
+  products: OrderProduct[]
+}
+
+type Props = {
+  title?: string
+  orders: Order[] | Promise<Order[]>
+  orderReturnAction({ id }: { id: string }): Promise<void>
+  orderTrackAction({ id }: { id: string }): Promise<void>
+  paginationInfo?: CursorPaginationInfo | Promise<CursorPaginationInfo>
+}
+
+export function OrderListSection({
+  title = 'Orders',
+  orders,
+  orderReturnAction,
+  orderTrackAction,
+  paginationInfo,
+}: Props) {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-xl">{title}</h1>
+      <div>
+        <OrderList orders={orders} />
+      </div>
+      {paginationInfo && <CursorPagination info={paginationInfo} />}
+    </div>
+  )
 }
