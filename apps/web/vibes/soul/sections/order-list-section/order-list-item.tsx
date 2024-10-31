c7820c5fc0bc9f1a0473c565ba69e9b0f27c9e92
Andrew Reifman <andrewr23@gmail.com>
2024-10-31 16:57:32 -0400

    chore: styling order history

--- apps/web/vibes/soul/sections/order-list-section/order-list-item.tsx
+++ apps/web/vibes/soul/sections/order-list-section/order-list-item.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 
 import { Badge } from '@/vibes/soul/primitives/badge'
+import { ButtonLink } from '@/vibes/soul/primitives/button-link'
 
 import { OrderListLineItem } from './order-list-line-item'
 
@@ -8,6 +9,7 @@ export type Order = {
   id: string
   totalPrice: string
   status: string
+  href: string
   lineItems: OrderListLineItem[]
 }
 
@@ -18,24 +20,30 @@ type Props = {
 
 export function OrderListItem({ className, order }: Props) {
   return (
-    <div className={clsx('border-t border-contrast-200 pb-10 pt-6 last:border-b', className)}>
-      <div className="mb-6 flex items-start gap-x-12">
-        <div>
-          <span className="font-mono text-xs uppercase leading-normal text-contrast-500">
-            Order #
-          </span>
-          <span className="block text-lg font-semibold leading-normal">{order.id}</span>
+    <div className={clsx('border-t border-contrast-200 pb-4 pt-6 last:border-b', className)}>
+      <div className="flex justify-between gap-x-10">
+        <div className="flex items-start gap-x-12">
+          <div>
+            <span className="font-mono text-xs uppercase leading-normal text-contrast-500">
+              Order #
+            </span>
+            <span className="block text-lg font-semibold leading-normal">{order.id}</span>
+          </div>
+          <div>
+            <span className="font-mono text-xs uppercase leading-normal text-contrast-500">
+              Total
+            </span>
+            <span className="block text-lg font-semibold leading-normal">{order.totalPrice}</span>
+          </div>
+          <Badge className="mt-0.5">{order.status}</Badge>
         </div>
-        <div>
-          <span className="font-mono text-xs uppercase leading-normal text-contrast-500">
-            Total
-          </span>
-          <span className="block text-lg font-semibold leading-normal">{order.totalPrice}</span>
-        </div>
-        <Badge className="mt-0.5">{order.status}</Badge>
+
+        <ButtonLink href={order.href} size="small">
+          View details
+        </ButtonLink>
       </div>
 
-      <div className="flex gap-4">
+      <div className="flex gap-4 overflow-x-auto py-6">
         {order.lineItems.map(lineItem => (
           <OrderListLineItem key={lineItem.id} lineItem={lineItem} />
         ))}
--- apps/web/vibes/soul/sections/order-list-section/order-list-line-item.tsx
+++ apps/web/vibes/soul/sections/order-list-section/order-list-line-item.tsx
@@ -21,42 +21,38 @@ type Props = {
 
 export function OrderListLineItem({ className, lineItem }: Props) {
   return (
-    <div className={clsx('@container', className)}>
-      <Link
-        id={lineItem.id}
-        href={lineItem.href}
-        className="group flex cursor-pointer flex-col gap-2 rounded-xl ring-primary ring-offset-4 focus-visible:outline-0 focus-visible:ring-2 @md:rounded-2xl"
-      >
-        <div className="relative aspect-square overflow-hidden rounded-[inherit] bg-contrast-100">
-          {lineItem.image?.src != null ? (
-            <Image
-              src={lineItem.image.src}
-              fill
-              sizes="(max-width: 768px) 70vw, 33vw"
-              alt="Category card image"
-              className="w-full scale-100 select-none bg-contrast-100 object-cover transition-transform duration-500 ease-out group-hover:scale-110"
-            />
-          ) : (
-            <div className="pl-2 pt-3 text-7xl font-bold leading-[0.8] tracking-tighter text-contrast-300 transition-transform duration-500 ease-out group-hover:scale-105">
-              {lineItem.title}
-            </div>
-          )}
-        </div>
-      </Link>
-      <div className="mt-2 flex flex-col items-start gap-x-4 gap-y-3 px-1 @xs:mt-3 @xs:flex-row">
-        <div className="flex-1">
-          <Link tabIndex={-1} href={lineItem.href} className="group text-base">
-            <span className="block font-semibold">{lineItem.title}</span>
-
-            {lineItem.subtitle != null && lineItem.subtitle !== '' && (
-              <span className="block text-sm font-normal text-contrast-400">
-                {lineItem.subtitle}
-              </span>
-            )}
-            {lineItem.price != null && <PriceLabel price={lineItem.price} />}
-          </Link>
-        </div>
+    <Link
+      id={lineItem.id}
+      href={lineItem.href}
+      className={clsx(
+        'group w-40 shrink-0 basis-40 cursor-pointer rounded-xl ring-primary ring-offset-4 focus-visible:outline-0 focus-visible:ring-2 @md:rounded-2xl',
+        className
+      )}
+    >
+      <div className="relative aspect-square overflow-hidden rounded-[inherit] bg-contrast-100">
+        {lineItem.image?.src != null ? (
+          <Image
+            src={lineItem.image.src}
+            fill
+            sizes="(max-width: 768px) 70vw, 33vw"
+            alt="Category card image"
+            className="w-full scale-100 select-none bg-contrast-100 object-cover transition-transform duration-500 ease-out group-hover:scale-110"
+          />
+        ) : (
+          <div className="pl-2 pt-3 text-7xl font-bold leading-[0.8] tracking-tighter text-contrast-300 transition-transform duration-500 ease-out group-hover:scale-105">
+            {lineItem.title}
+          </div>
+        )}
       </div>
-    </div>
+
+      <div className="mt-2 px-1 text-sm @xs:mt-3">
+        <span className="block font-semibold">{lineItem.title}</span>
+
+        {lineItem.subtitle != null && lineItem.subtitle !== '' && (
+          <span className="mb-1 block font-normal text-contrast-400">{lineItem.subtitle}</span>
+        )}
+        {lineItem.price != null && <PriceLabel price={lineItem.price} />}
+      </div>
+    </Link>
   )
 }
