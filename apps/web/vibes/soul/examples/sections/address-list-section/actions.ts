00a57cc506a25f537f6799d54b5b463a76e5787a
Alan Pledger <apledger3@gmail.com>
2024-11-02 10:24:27 -0400

    feat: add optimistic for addresses

--- apps/web/vibes/soul/examples/sections/address-list-section/actions.ts
+++ apps/web/vibes/soul/examples/sections/address-list-section/actions.ts
@@ -1,18 +1,19 @@
 import { SubmissionResult } from '@conform-to/react'
 import { parseWithZod } from '@conform-to/zod'
+import { randomUUID } from 'crypto'
 import { z } from 'zod'
 
-import { schema } from '@/vibes/soul/sections/address-list-section/schema'
+import { addressSchema, idSchema } from '@/vibes/soul/sections/address-list-section/schema'
 
-type Address = z.infer<typeof schema>
+type Address = z.infer<typeof addressSchema>
 
 export async function createAddressAction(
-  prevState: Awaited<{ address: Address; lastResult: SubmissionResult | null }>,
+  prevState: Awaited<{ address: Address | null; lastResult: SubmissionResult | null }>,
   formData: FormData
-): Promise<{ lastResult: SubmissionResult; address: Address }> {
+): Promise<{ lastResult: SubmissionResult; address: Address | null }> {
   'use server'
 
-  const submission = parseWithZod(formData, { schema })
+  const submission = parseWithZod(formData, { schema: addressSchema })
 
   console.log('add', { submission })
 
@@ -22,7 +23,10 @@ export async function createAddressAction(
 
   await new Promise(resolve => setTimeout(resolve, 1000))
 
-  return { lastResult: submission.reply({ resetForm: true }), address: submission.value }
+  return {
+    lastResult: submission.reply({ resetForm: true }),
+    address: { ...submission.value, id: randomUUID() },
+  }
 }
 
 export async function updateAddressAction(
@@ -31,7 +35,7 @@ export async function updateAddressAction(
 ): Promise<{ lastResult: SubmissionResult; address: Address }> {
   'use server'
 
-  const submission = parseWithZod(formData, { schema })
+  const submission = parseWithZod(formData, { schema: addressSchema })
 
   console.log('update', { submission })
 
@@ -40,6 +44,8 @@ export async function updateAddressAction(
   }
 
   await new Promise(resolve => setTimeout(resolve, 1000))
+
+  // return { lastResult: submission.reply({ formErrors: ['Boom!'] }), address: prevState.address }
 
   return { lastResult: submission.reply({ resetForm: true }), address: submission.value }
 }
@@ -50,7 +56,7 @@ export async function deleteAddressAction(
 ): Promise<{ lastResult: SubmissionResult; id: string }> {
   'use server'
 
-  const submission = parseWithZod(formData, { schema: z.object({ id: z.string() }) })
+  const submission = parseWithZod(formData, { schema: idSchema })
 
   console.log('delete', { submission })
 
@@ -69,7 +75,7 @@ export async function setDefaultAddressAction(
 ): Promise<{ lastResult: SubmissionResult; id: string }> {
   'use server'
 
-  const submission = parseWithZod(formData, { schema: z.object({ id: z.string() }) })
+  const submission = parseWithZod(formData, { schema: idSchema })
 
   console.log('set default address', { submission })
 
