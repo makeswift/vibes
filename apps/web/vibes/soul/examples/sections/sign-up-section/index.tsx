34dd9d93f032a936b44f16262e71cb6b44cae348
Alan Pledger <apledger3@gmail.com>
2024-11-03 14:20:09 -0500

    feat: add sign up section

---
+++ apps/web/vibes/soul/examples/sections/sign-up-section/index.tsx
@@ -0,0 +1,7 @@
+import { SignUpSection } from '@/vibes/soul/sections/sign-up-section'
+
+import { signUpAction } from './actions'
+
+export default function Preview() {
+  return <SignUpSection action={signUpAction} />
+}
