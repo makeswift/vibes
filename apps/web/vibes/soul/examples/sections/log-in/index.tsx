cca930ba8b99d33a50f59567224cf979eab0ae3a
Alan Pledger <apledger3@gmail.com>
2024-11-03 14:43:12 -0500

    feat: add sign in section

--- apps/web/vibes/soul/examples/sections/log-in/index.tsx
+++
@@ -1,5 +0,0 @@
-import { LogIn } from '@/vibes/soul/sections/log-in'
-
-export default function Preview() {
-  return <LogIn />
-}
---
+++ apps/web/vibes/soul/examples/sections/sign-in-section/actions.ts
@@ -0,0 +1,21 @@
+import { SubmissionResult } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+
+import { schema } from '@/vibes/soul/sections/sign-in-section/schema'
+
+export async function signInAction(lastResult: SubmissionResult | null, formData: FormData) {
+  'use server'
+
+  const submission = parseWithZod(formData, { schema })
+
+  if (submission.status !== 'success') {
+    return submission.reply({ formErrors: ['Boom!'] })
+  }
+
+  // Simulate a network request
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  // const user = await logIn(submission.value)
+
+  return submission.reply({ resetForm: true })
+}
