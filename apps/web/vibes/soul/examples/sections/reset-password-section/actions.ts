2814abe355e7a483b141bc35f53d403f4cc5bb35
Alan Pledger <apledger3@gmail.com>
2024-11-03 14:33:41 -0500

    feat: add reset password section

---
+++ apps/web/vibes/soul/examples/sections/reset-password-section/actions.ts
@@ -0,0 +1,21 @@
+import { SubmissionResult } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+
+import { schema } from '@/vibes/soul/sections/reset-password-section/schema'
+
+export async function resetPasswordAction(lastResult: SubmissionResult | null, formData: FormData) {
+  'use server'
+
+  const submission = parseWithZod(formData, { schema })
+
+  if (submission.status !== 'success') {
+    return submission.reply({ formErrors: ['Boom!'] })
+  }
+
+  // Simulate a network request
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  // const user = await logIn(submission.value)
+
+  return submission.reply({ resetForm: true })
+}
