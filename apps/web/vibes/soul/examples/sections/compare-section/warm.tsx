0f0d2b8f5fe83aa3e79644adf0908ba72fa8e98a
Alan Pledger <apledger3@gmail.com>
2024-11-03 12:38:54 -0500

    feat: add compare section to nav

---
+++ apps/web/vibes/soul/examples/sections/compare-section/warm.tsx
@@ -0,0 +1,18 @@
+import { getProducts } from '@/vibes/soul/data'
+import { CompareSection } from '@/vibes/soul/sections/compare-section'
+
+import { addToCartAction } from './actions'
+
+const products = getProducts('Warm')
+
+export default async function Preview({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] }>
+}) {
+  return (
+    <div className="p-20">
+      <CompareSection products={products} addToCartAction={addToCartAction} />
+    </div>
+  )
+}
