fcd7287d2237d953a767b53887a911f58df1ad18
Alan Pledger <apledger3@gmail.com>
2024-11-03 21:04:23 -0500

    fix: product description styles

--- apps/web/vibes/soul/sections/product-description/index.tsx
+++ apps/web/vibes/soul/sections/product-description/index.tsx
@@ -19,20 +19,19 @@ export interface Props {
 export function ProductDescription({ accordions, image, video }: Readonly<Props>) {
   return (
     <div className="bg-background @container">
-      <div className="relative mx-auto flex w-full max-w-screen-2xl flex-col-reverse items-start justify-between gap-x-4 gap-y-4 py-6 @lg:flex-row @lg:px-6 @lg:py-24 @xl:gap-x-10 @5xl:px-20 @7xl:gap-x-32">
-        <Accordions className="px-5 @lg:sticky @lg:top-20" type="multiple">
-          {accordions.map((accordion, index) => (
-            <Accordion title={accordion.title} value={index.toString()}>
-              {accordion.content}
-            </Accordion>
-          ))}
-        </Accordions>
+      <div className="relative mx-auto flex w-full max-w-screen-2xl flex-col-reverse items-start justify-between gap-x-4 gap-y-4 py-6 @xl:flex-row @xl:gap-x-10 @xl:px-6 @xl:py-24 @5xl:px-20 @7xl:gap-x-32">
+        <div className="w-full px-4 @xl:sticky @xl:top-20 @xl:basis-1/2 @xl:px-0">
+          <Accordions type="multiple">
+            {accordions.map((accordion, index) => (
+              <Accordion key={index} title={accordion.title} value={index.toString()}>
+                {accordion.content}
+              </Accordion>
+            ))}
+          </Accordions>
+        </div>
 
         {/* Image || Video Container */}
-        <div
-          className="relative aspect-square w-full overflow-hidden 
-            @lg:sticky @lg:top-20 @lg:aspect-[9/12] @lg:rounded-2xl @4xl:min-w-96"
-        >
+        <div className="relative aspect-square w-full overflow-hidden @xl:aspect-[9/12] @xl:basis-1/2 @xl:rounded-2xl">
           {image ? (
             <Image
               src={image.src}
