e55e27f760c507fe8ee6b82e9e7aeb78cb2f5b3b
Andrew Reifman <andrewr23@gmail.com>
2024-11-05 00:44:50 -0500

    fix: not found section spacing

--- apps/web/vibes/soul/sections/not-found/index.tsx
+++ apps/web/vibes/soul/sections/not-found/index.tsx
@@ -1,11 +1,11 @@
 export const NotFound = function NotFound() {
   return (
     <section className="@container">
-      <div className="mx-auto flex max-w-4xl flex-col gap-4 px-3 pb-16 pt-40 @xl:px-6 @5xl:px-20 @5xl:pt-48">
-        <h1 className="text-2xl font-medium @4xl:text-3xl">
+      <div className="mx-auto max-w-3xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
+        <h1 className="mb-3 font-heading text-3xl font-medium leading-none @xl:text-4xl @4xl:text-5xl">
           We can&apos;t find the page you&apos;re looking for.
         </h1>
-        <p className="@4xl:text-lg">
+        <p className="text-lg text-contrast-500">
           But while you&apos;re here, why don&apos;t you have a look around.
         </p>
       </div>
