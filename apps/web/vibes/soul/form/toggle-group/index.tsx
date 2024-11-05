d6aaacbf5449ca2d3872875a5c1ac8489c3028df
Alan Pledger <apledger3@gmail.com>
2024-11-05 12:56:10 -0500

    feat: add radio form primitives

--- apps/web/vibes/soul/form/toggle-group/index.tsx
+++ apps/web/vibes/soul/form/toggle-group/index.tsx
@@ -29,7 +29,7 @@ export const ToggleGroup = React.forwardRef<
         {...rest}
         ref={ref}
         aria-label={label}
-        className="flex flex-wrap gap-x-1 gap-y-2"
+        className="flex flex-wrap gap-2"
       >
         {options.map(option => (
           <ToggleGroupPrimitive.Item
@@ -37,7 +37,7 @@ export const ToggleGroup = React.forwardRef<
             value={option.value}
             aria-label={option.label}
             disabled={option.disabled}
-            className="whitespace-nowrap rounded-full border border-contrast-100 px-3 py-1.5 font-body text-sm font-normal leading-normal ring-primary transition-colors focus-visible:outline-0 focus-visible:ring-2 data-[disabled]:cursor-not-allowed data-[state=on]:border-foreground data-[state=off]:bg-background data-[state=on]:bg-foreground data-[state=on]:text-background data-[disabled]:opacity-50 data-[disabled]:hover:border-transparent data-[state=off]:hover:border-contrast-200 data-[state=off]:hover:bg-contrast-100"
+            className="h-12 whitespace-nowrap rounded-full border border-contrast-100 px-4 font-body text-sm font-normal leading-normal ring-primary transition-colors focus-visible:outline-0 focus-visible:ring-2 data-[disabled]:pointer-events-none data-[state=on]:border-foreground data-[state=off]:bg-background data-[state=on]:bg-foreground data-[state=on]:text-background data-[disabled]:opacity-50 data-[disabled]:hover:border-transparent data-[state=off]:hover:border-contrast-200 data-[state=off]:hover:bg-contrast-100"
           >
             {option.label}
           </ToggleGroupPrimitive.Item>
