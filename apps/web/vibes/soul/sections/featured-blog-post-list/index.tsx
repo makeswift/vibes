3d4243cc2272c7e7f6ad8f62ccbedcbf0e2f039d
Andrew Reifman <andrewr23@gmail.com>
2024-11-05 00:27:23 -0500

    fix: featured blog list section spacing

--- apps/web/vibes/soul/sections/featured-blog-post-list/index.tsx
+++ apps/web/vibes/soul/sections/featured-blog-post-list/index.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link'
-
 import { BlogPost } from '@/vibes/soul/primitives/blog-post-card'
-import { Button } from '@/vibes/soul/primitives/button'
+import { ButtonLink } from '@/vibes/soul/primitives/button-link'
 import { BlogPostList } from '@/vibes/soul/sections/blog-post-list'
 
 interface Link {
@@ -23,19 +21,22 @@ export const FeaturedBlogPostList = function FeaturedBlogPostList({
   posts,
 }: Props) {
   return (
-    <section className="bg-background @container">
-      <div className="relative mx-auto max-w-screen-2xl px-3 pb-20 pt-24 @xl:px-6 @4xl:pt-28 @5xl:px-20">
-        <h2 className="mb-2 font-heading text-3xl font-semibold leading-none text-foreground @4xl:text-6xl @4xl:font-medium">
+    <section className="@container">
+      <div className="mx-auto max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
+        <h1 className="mb-3 font-heading text-4xl font-medium leading-none text-foreground @xl:text-5xl @4xl:text-6xl">
           {title}
-        </h2>
+        </h1>
+
         {description != null && description !== '' && (
-          <p className="max-w-md text-foreground">{description}</p>
+          <p className="max-w-lg text-lg text-contrast-500">{description}</p>
         )}
-        <BlogPostList posts={posts} className="mt-6 @4xl:mt-8" />
+
+        <BlogPostList posts={posts} className="mb-8 mt-8 @4xl:mb-10 @4xl:mt-10" />
+
         {cta != null && cta.href !== '' && cta.label !== '' && (
-          <Button className="mx-auto mt-12 bg-primary @4xl:mt-16" asChild>
-            <Link href={cta.href}>{cta.label}</Link>
-          </Button>
+          <ButtonLink variant="tertiary" size="medium" href={cta.href}>
+            {cta.label}
+          </ButtonLink>
         )}
       </div>
     </section>
