f4980a4ec52f503928102c82d8179e8b1d415674
Alan Pledger <apledger3@gmail.com>
2024-11-06 13:05:34 -0500

    feat: add props to not found

--- apps/web/vibes/soul/sections/not-found/index.tsx
+++ apps/web/vibes/soul/sections/not-found/index.tsx
@@ -1,13 +1,19 @@
-export const NotFound = function NotFound() {
+type Props = {
+  title?: string
+  subtitle?: string
+}
+
+export function NotFound({
+  title = 'Not found',
+  subtitle = "Take a look around if you're lost.",
+}: Props) {
   return (
     <section className="@container">
       <div className="mx-auto max-w-3xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
         <h1 className="mb-3 font-heading text-3xl font-medium leading-none @xl:text-4xl @4xl:text-5xl">
-          We can&apos;t find the page you&apos;re looking for.
+          {title}
         </h1>
-        <p className="text-lg text-contrast-500">
-          But while you&apos;re here, why don&apos;t you have a look around.
-        </p>
+        <p className="text-lg text-contrast-500">{subtitle}</p>
       </div>
     </section>
   )
