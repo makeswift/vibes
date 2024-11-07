7b0aa9da1e25f73b088813c5b6ae7f93c888c82e
Andrew Reifman <andrewr23@gmail.com>
2024-11-07 01:52:24 -0500

    chore: style featured products section

--- apps/web/vibes/soul/sections/featured-products-list/index.tsx
+++ apps/web/vibes/soul/sections/featured-products-list/index.tsx
@@ -21,28 +21,27 @@ export function FeaturedProductsList({
 }: FeaturedProductsListProps) {
   return (
     <section className="@container">
-      <div className="relative mx-auto flex max-w-screen-2xl flex-col gap-6 px-4 py-10 @4xl:flex-row @4xl:px-8 @4xl:py-24">
-        <div className="top-28 flex w-full flex-col items-start justify-between gap-4 self-start @xl:flex-row @4xl:sticky @4xl:w-1/2 @4xl:max-w-md @4xl:flex-col @4xl:items-start @4xl:justify-start @5xl:px-0 @6xl:w-4/12">
-          <div>
-            <h2 className="mb-4 font-heading text-4xl font-semibold leading-none text-foreground @4xl:text-6xl @4xl:font-medium">
+      <div className="mx-auto flex max-w-screen-2xl flex-col items-stretch gap-x-16 gap-y-10 px-4 py-10 @xl:px-6 @xl:py-14 @4xl:flex-row @4xl:px-8 @4xl:py-20">
+        <div className="@4xl:w-1/3">
+          <div className="sticky top-10">
+            <h2 className="mb-3 font-heading text-4xl font-medium leading-none text-foreground @4xl:text-5xl">
               {title}
             </h2>
             {description != null && description !== '' && (
-              <p className="mt-1.5 max-w-md pb-2 text-lg font-light text-foreground">
+              <p className="mb-8 max-w-xl text-lg font-light leading-normal text-foreground">
                 {description}
               </p>
             )}
-          </div>
-          {cta?.href != null && cta.href !== '' && cta.label !== '' && (
-            <div className="shrink-0">
+
+            {cta?.href != null && cta.href !== '' && cta.label !== '' && (
               <ButtonLink href={cta.href} variant="secondary">
                 {cta.label}
               </ButtonLink>
-            </div>
-          )}
+            )}
+          </div>
         </div>
 
-        <ProductsList products={products} className="4xl:w-1/2 @6xl:w-8/12" />
+        <ProductsList products={products} className="flex-1" />
       </div>
     </section>
   )
