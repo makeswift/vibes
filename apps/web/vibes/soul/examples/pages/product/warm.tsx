b2933b9b87e8cb284e426ea301cbfff863ada4cb
Alan Pledger <apledger3@gmail.com>
2024-11-07 16:35:54 -0500

    fix: subscribe actions

--- apps/web/vibes/soul/examples/pages/product/warm.tsx
+++ apps/web/vibes/soul/examples/pages/product/warm.tsx
@@ -1,5 +1,6 @@
 import { getProducts } from '@/vibes/soul/data'
 import { locales } from '@/vibes/soul/data/locales'
+import { action as subscribeAction } from '@/vibes/soul/examples/primitives/inline-email-form/actions'
 import { localeAction } from '@/vibes/soul/examples/primitives/navigation/actions'
 import { navigationLinks } from '@/vibes/soul/examples/primitives/navigation/warm'
 import { footerLinks, logo } from '@/vibes/soul/examples/sections/footer/warm'
@@ -75,7 +76,7 @@ export default function Preview() {
         <Subscribe
           title="Sign up for our newsletter"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-          action={action}
+          action={subscribeAction}
         />
 
         <Footer sections={footerLinks} logo="SOUL" />
