270a83cd1cd54837c173dcb518515f43adc327fa
Alan Pledger <apledger3@gmail.com>
2024-11-08 17:45:02 -0500

    feat: add breadcrumbs to PDP

--- apps/web/vibes/soul/sections/product-description/index.tsx
+++ apps/web/vibes/soul/sections/product-description/index.tsx
@@ -18,8 +18,8 @@ type Props = {
 export function ProductDescription({ accordions, image }: Props) {
   return (
     <div className="@container">
-      <div className="mx-auto flex w-full max-w-screen-xl flex-col-reverse items-stretch gap-x-10 gap-y-6 px-4 py-10 @xl:px-6 @xl:py-14 @2xl:flex-row @2xl:gap-x-12 @4xl:gap-x-16 @4xl:px-8 @4xl:py-20">
-        <div className="flex-1">
+      <div className="mx-auto flex w-full max-w-screen-lg flex-col items-stretch px-4 py-10 @xl:px-6 @xl:py-14 @2xl:flex-row @4xl:px-8 @4xl:py-20">
+        <div className="w-full pb-5 @2xl:w-1/2 @2xl:basis-1/2 @2xl:pr-6 @4xl:pr-8">
           <Accordions type="multiple" className="sticky top-6">
             {accordions.map((accordion, index) => (
               <Accordion key={index} title={accordion.title} value={index.toString()}>
@@ -29,16 +29,18 @@ export function ProductDescription({ accordions, image }: Props) {
           </Accordions>
         </div>
 
-        <div className="relative aspect-square w-full flex-1 self-start overflow-hidden rounded-2xl @2xl:aspect-[4/5]">
-          {image && (
-            <Image
-              src={image.src}
-              fill
-              alt={image.alt}
-              sizes="(max-width: 500px) 100vw, 50vw"
-              className="object-cover"
-            />
-          )}
+        <div className="w-full self-start pt-5 @2xl:w-1/2 @2xl:basis-1/2 @2xl:pl-6 @4xl:pl-8">
+          <div className="relative aspect-square w-full overflow-hidden rounded-2xl @2xl:aspect-[4/5]">
+            {image && (
+              <Image
+                src={image.src}
+                fill
+                alt={image.alt}
+                sizes="(max-width: 500px) 100vw, 50vw"
+                className="object-cover"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
