631c0bb0d7af27f8fa6356139ee3aacfbd068c97
Alan Pledger <apledger3@gmail.com>
2024-11-09 14:21:44 -0500

    fix: make container size optional

--- apps/web/vibes/soul/sections/sticky-sidebar-layout/index.tsx
+++ apps/web/vibes/soul/sections/sticky-sidebar-layout/index.tsx
@@ -11,7 +11,7 @@ export function StickySidebarLayout({
   className?: string
   sidebar: React.ReactNode
   children: React.ReactNode
-  containerSize: 'lg' | 'xl' | '2xl'
+  containerSize?: 'lg' | 'xl' | '2xl'
   sidebarSize?: '1/4' | '1/3' | '1/2' | 'small' | 'medium' | 'large'
   sidebarPosition?: 'left' | 'right'
 }) {
