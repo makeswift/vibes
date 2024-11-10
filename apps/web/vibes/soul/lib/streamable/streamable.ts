d1fcaa3860b58816beffc7f236a4a83095ecf4c4
Miguel Oller <miguel@makeswift.com>
2024-11-10 00:20:05 -0500

    refactor: move `mapStreamable` to server-only file
    React's `use` should only be used with promises passed from server
    components. Promises created in a client component will result in an
    infinite render loop since `use` will encounter a new promise each time.
    Calling `.then` results in creating a new promise, so we needed to make
    sure `mapStreamable` was only called in the server to avoid creating a
    new promise on the client. Furthermore, promises passed from the server
    to the client are converted to a `ReactPromise` by React, which returns
    `undefined` when the `.then` method is called, instead of a new
    `Promise`. Probably to avoid the pitfall of creating a new promise on
    each render.

--- apps/web/vibes/soul/lib/streamable/streamable.ts
+++ apps/web/vibes/soul/lib/streamable/streamable.ts
@@ -5,10 +5,3 @@ export type Streamable<T> = T | Promise<T>
 export function useStreamable<T>(streamable: Streamable<T>): T {
   return streamable instanceof Promise ? use(streamable) : streamable
 }
-
-export function mapStreamable<T, U>(
-  streamable: Streamable<T>,
-  mapper: (value: T) => U
-): Streamable<U> {
-  return streamable instanceof Promise ? streamable.then(mapper) : mapper(streamable)
-}
