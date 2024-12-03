739382706b25c0766e1c85ab0e9f91ba54081f03
jamesqquick <james.q.quick@gmail.com>
2024-12-03 09:55:44 -0600

    chore: updated APIKey to use public key and sitemap config to ignore preview paths

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -5,4 +5,20 @@ const dev = process.env.NODE_ENV !== 'production'
 module.exports = {
   siteUrl: dev ? 'http://localhost:3000' : 'https://vibes.site/',
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    // ignore preview pages
+    if (path.includes('/preview/')) {
+      console.log('ignoring path', path)
+      return null
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 }
