3dca94395f2872aea86d18a0f207bf5665ab0707
Jorge Moya <jorge.moya@bigcommerce.com>
2024-12-04 10:52:33 -0600

    fix: compute banner height instead of using state

--- apps/web/vibes/soul/sections/header-section/index.tsx
+++ apps/web/vibes/soul/sections/header-section/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { forwardRef, useEffect, useState } from 'react'
+import { forwardRef, useState } from 'react'
 import Headroom from 'react-headroom'
 
 import { Banner } from '@/vibes/soul/primitives/banner'
@@ -14,22 +14,13 @@ interface Props {
 export const HeaderSection = forwardRef<React.ComponentRef<'div'>, Props>(
   ({ navigation, banner }, ref) => {
     const [bannerElement, setBannerElement] = useState<HTMLElement | null>(null)
-    const [bannerHeight, setBannerHeight] = useState(0)
     const [isFloating, setIsFloating] = useState(false)
-
-    useEffect(() => {
-      if (bannerElement) {
-        setBannerHeight(bannerElement.getBoundingClientRect().height)
-      }
-    }, [bannerElement])
 
     return (
       <div ref={ref}>
-        {banner && (
-          <Banner ref={setBannerElement} {...banner} onDismiss={() => setBannerHeight(0)} />
-        )}
+        {banner && <Banner ref={setBannerElement} {...banner} />}
         <Headroom
-          pinStart={bannerHeight}
+          pinStart={bannerElement?.getBoundingClientRect().height}
           onUnpin={() => setIsFloating(true)}
           onUnfix={() => setIsFloating(false)}
         >
