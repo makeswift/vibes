96b342d786bf596e3758052aa9ddd00eb82ff3f6
Miguel Oller <miguel@makeswift.com>
2024-12-05 14:24:49 -0500

    feat: update Prettier config to match Catalyst

---
+++ apps/web/prettier.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['cn'],
+}
+
+module.exports = config
