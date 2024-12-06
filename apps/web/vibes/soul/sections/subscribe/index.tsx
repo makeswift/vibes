4d20e08ac82ea1a56f62e19208829a9340e5a611
Jorge Moya <jorge.moya@bigcommerce.com>
2024-12-06 15:27:45 -0600

    fix: remove toast in favor of FormStatus

--- apps/web/vibes/soul/sections/subscribe/index.tsx
+++ apps/web/vibes/soul/sections/subscribe/index.tsx
@@ -12,14 +12,12 @@ export function Subscribe({
   title,
   description,
   placeholder,
-  dismissLabel,
 }: {
   action: Action<SubmissionResult | null, FormData>;
   image?: { src: string; alt: string };
   title: string;
   description?: string;
   placeholder?: string;
-  dismissLabel?: string;
 }) {
   return (
     <section className="bg-primary-shadow @container">
@@ -40,9 +38,7 @@ export function Subscribe({
           <div
             className={clsx(
               'flex w-full flex-col gap-10 px-4 py-10 @xl:px-6 @xl:py-14 @4xl:gap-16 @4xl:px-8 @4xl:py-20',
-              image != null
-                ? '@4xl:max-w-4xl'
-                : 'mx-auto max-w-screen-2xl @4xl:flex-row @4xl:items-center',
+              image != null ? '@4xl:max-w-4xl' : 'mx-auto max-w-screen-2xl @4xl:flex-row',
             )}
           >
             <div className="flex-1">
@@ -51,12 +47,7 @@ export function Subscribe({
               </h2>
               <p className="text-primary-highlight opacity-75">{description}</p>
             </div>
-            <InlineEmailForm
-              action={action}
-              className="flex-1"
-              dismissLabel={dismissLabel}
-              placeholder={placeholder}
-            />
+            <InlineEmailForm action={action} className="flex-1" placeholder={placeholder} />
           </div>
         </div>
       </div>
