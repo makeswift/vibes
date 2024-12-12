5884c25f861ed007f750572ddac23e896f198bb6
Alan Pledger <apledger3@gmail.com>
2024-12-12 16:40:06 -0500

    refactor: use dynamic form in sign up section

--- apps/web/vibes/soul/sections/sign-up-section/index.tsx
+++ apps/web/vibes/soul/sections/sign-up-section/index.tsx
@@ -1,40 +1,25 @@
-import { SignUpAction, SignUpForm } from './sign-up-form';
+import { DynamicForm, DynamicFormAction } from '@/vibes/soul/primitives/dynamic-form';
+import { Field, FieldGroup } from '@/vibes/soul/primitives/dynamic-form/schema';
 
-interface Props {
+interface Props<F extends Field> {
   title?: string;
-  action: SignUpAction;
+  action: DynamicFormAction<F>;
+  fields: (F | FieldGroup<F>)[];
   submitLabel?: string;
-  firstNameLabel?: string;
-  lastNameLabel?: string;
-  emailLabel?: string;
-  passwordLabel?: string;
-  confirmPasswordLabel?: string;
 }
 
-export function SignUpSection({
+export function SignUpSection<F extends Field>({
   title = 'Create Account',
+  fields,
   submitLabel,
-  firstNameLabel,
-  lastNameLabel,
-  emailLabel,
-  passwordLabel,
-  confirmPasswordLabel,
   action,
-}: Props) {
+}: Props<F>) {
   return (
     <div className="@container">
       <div className="flex flex-col justify-center gap-y-24 px-3 py-10 @xl:flex-row @xl:px-6 @4xl:py-20 @5xl:px-20">
         <div className="flex w-full flex-col @xl:max-w-lg @xl:pr-10 @4xl:pr-20">
           <h1 className="mb-10 text-4xl font-medium leading-none @xl:text-5xl">{title}</h1>
-          <SignUpForm
-            action={action}
-            confirmPasswordLabel={confirmPasswordLabel}
-            emailLabel={emailLabel}
-            firstNameLabel={firstNameLabel}
-            lastNameLabel={lastNameLabel}
-            passwordLabel={passwordLabel}
-            submitLabel={submitLabel}
-          />
+          <DynamicForm action={action} fields={fields} submitLabel={submitLabel} />
         </div>
       </div>
     </div>
