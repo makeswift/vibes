a2ff820ac06ef35d55cb317f0a2e9def2aa890d3
Alan Pledger <apledger3@gmail.com>
2024-12-17 16:39:51 -0500

    refactor: move default css variables into components

--- apps/web/vibes/soul/primitives/card/index.tsx
+++ apps/web/vibes/soul/primitives/card/index.tsx
@@ -13,6 +13,23 @@ export interface CardProps {
   aspectRatio?: '5:6' | '3:4' | '1:1';
 }
 
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --card-focus: hsl(var(--primary));
+ *   --card-border-radius: 1rem;
+ *   --card-light-text: hsl(var(--foreground));
+ *   --card-light-icon: hsl(var(--foreground));
+ *   --card-light-background: hsl(var(--contrast-100));
+ *   --card-dark-text: hsl(var(--background));
+ *   --card-dark-icon: hsl(var(--background));
+ *   --card-dark-background: hsl(var(--contrast-500));
+ * }
+ * ```
+ */
 export function Card({
   className,
   title,
@@ -25,7 +42,7 @@ export function Card({
   return (
     <Link
       className={clsx(
-        'group relative flex min-w-0 cursor-pointer flex-col gap-2 rounded-[var(--card-border-radius)] @container focus:outline-0 focus-visible:outline-0',
+        'group relative flex min-w-0 cursor-pointer flex-col gap-2 rounded-[var(--card-border-radius,1rem)] @container focus:outline-0 focus-visible:outline-0',
         className,
       )}
       href={href}
@@ -34,18 +51,18 @@ export function Card({
         className={clsx(
           'absolute right-5 top-5 z-10 transition-transform duration-700 ease-out group-hover:-translate-y-1.5 group-hover:translate-x-1.5',
           {
-            light: 'text-[var(--card-light-icon)]',
-            dark: 'text-[var(--card-dark-icon)]',
+            light: 'text-[var(--card-light-icon,hsl(var(--foreground)))]',
+            dark: 'text-[var(--card-dark-icon,hsl(var(--background)))]',
           }[iconColorScheme],
         )}
         strokeWidth={1.5}
       />
       <div
         className={clsx(
-          'relative overflow-hidden rounded-[inherit] group-focus:ring-[var(--card-focus)] group-focus-visible:ring-2',
+          'relative overflow-hidden rounded-[inherit] group-focus:ring-[var(--card-focus,hsl(var(--primary)))] group-focus-visible:ring-2',
           {
-            light: 'bg-[var(--card-light-background)]',
-            dark: 'bg-[var(--card-dark-background)]',
+            light: 'bg-[var(--card-light-background,hsl(var(--contrast-100)))]',
+            dark: 'bg-[var(--card-dark-background,hsl(var(--contrast-500)))]',
           }[textColorScheme],
           {
             '5:6': 'aspect-[5/6]',
@@ -60,8 +77,8 @@ export function Card({
             className={clsx(
               'w-full scale-100 select-none object-cover transition-transform duration-500 ease-out group-hover:scale-110',
               {
-                light: 'bg-[var(--card-light-background)]',
-                dark: 'bg-[var(--card-dark-background)]',
+                light: 'bg-[var(--card-light-background,hsl(var(--contrast-100)))]',
+                dark: 'bg-[var(--card-dark-background,hsl(var(--contrast-500)))]',
               }[textColorScheme],
             )}
             fill
@@ -73,8 +90,8 @@ export function Card({
             className={clsx(
               'break-words pl-5 pt-5 text-4xl font-bold leading-[0.8] tracking-tighter opacity-25 transition-transform duration-500 ease-out group-hover:scale-105 @xs:text-7xl',
               {
-                light: 'text-[var(--card-light-text)]',
-                dark: 'text-[var(--card-dark-text)]',
+                light: 'text-[var(--card-light-text,hsl(var(--foreground)))]',
+                dark: 'text-[var(--card-dark-text,hsl(var(--background)))]',
               }[textColorScheme],
             )}
           >
@@ -86,8 +103,8 @@ export function Card({
         className={clsx(
           'line-clamp-1 text-lg font-medium',
           {
-            light: 'text-[var(--card-light-text)]',
-            dark: 'text-[var(--card-dark-text)]',
+            light: 'text-[var(--card-light-text,hsl(var(--foreground)))]',
+            dark: 'text-[var(--card-dark-text,hsl(var(--background)))]',
           }[textColorScheme],
         )}
       >
