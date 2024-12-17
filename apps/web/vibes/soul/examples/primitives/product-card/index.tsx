2a0f05b23545ca8e5418b896c97fdead8a934343
Alan Pledger <apledger3@gmail.com>
2024-12-17 13:33:42 -0500

    feat: add CSS variables to ProductCard

--- apps/web/vibes/soul/examples/primitives/product-card/index.tsx
+++ apps/web/vibes/soul/examples/primitives/product-card/index.tsx
@@ -2,35 +2,120 @@ import { ProductCard } from '@/vibes/soul/primitives/product-card';
 
 export default function Preview() {
   return (
-    <div className="flex h-screen items-center justify-center gap-8 bg-background p-8">
-      <ProductCard
-        className="w-80"
-        product={{
-          id: '1',
-          href: '#',
-          title: 'Product Name',
-          image: {
-            src: 'https://rstr.in/monogram/vibes/oUL4h2FvqIl',
-            alt: 'Product Name',
-          },
-          subtitle: 'Blue/Black/Green',
-          price: '123.99',
-          badge: 'New',
-        }}
-        showCompare
-      />
-      <ProductCard
-        className="w-80"
-        product={{
-          id: '1',
-          href: '#',
-          title: 'Product Name',
-          subtitle: 'Blue/Black/Green',
-          price: '123.99',
-          badge: 'New',
-        }}
-        showCompare
-      />
+    <div>
+      <div className="bg-background p-8 @container">
+        <div className="m-auto flex max-w-screen-md flex-col items-center gap-8 @md:flex-row">
+          <ProductCard
+            className="w-full"
+            product={{
+              id: '1',
+              href: '#',
+              title: 'Product Name',
+              image: {
+                src: 'https://rstr.in/monogram/vibes/oUL4h2FvqIl',
+                alt: 'Product Name',
+              },
+              subtitle: 'Blue/Black/Green',
+              price: '$123.99',
+              badge: 'New',
+            }}
+            showCompare
+          />
+          <ProductCard
+            className="w-full"
+            product={{
+              id: '1',
+              href: '#',
+              title: 'Product Name',
+              subtitle: 'Blue/Black/Green',
+              price: {
+                type: 'sale',
+                previousValue: '$123.99',
+                currentValue: '$99.99',
+              },
+              badge: 'New',
+            }}
+            showCompare
+          />
+          <ProductCard
+            className="w-full"
+            product={{
+              id: '1',
+              href: '#',
+              title: 'Product Name',
+              image: {
+                src: 'https://rstr.in/monogram/vibes/oUL4h2FvqIl',
+                alt: 'Product Name',
+              },
+              subtitle: 'Blue/Black/Green',
+              price: {
+                type: 'range',
+                minValue: '$99.99',
+                maxValue: '$123.99',
+              },
+            }}
+            showCompare
+          />
+        </div>
+      </div>
+      <div className="bg-foreground p-8 @container">
+        <div className="m-auto flex max-w-screen-md flex-col items-center gap-8 @md:flex-row">
+          <ProductCard
+            className="w-full"
+            product={{
+              id: '1',
+              href: '#',
+              title: 'Product Name',
+              image: {
+                src: 'https://rstr.in/monogram/vibes/oUL4h2FvqIl',
+                alt: 'Product Name',
+              },
+              subtitle: 'Blue/Black/Green',
+              price: '$123.99',
+              badge: 'New',
+            }}
+            colorScheme="dark"
+            showCompare
+          />
+          <ProductCard
+            className="w-full"
+            product={{
+              id: '1',
+              href: '#',
+              title: 'Product Name',
+              subtitle: 'Blue/Black/Green',
+              price: {
+                type: 'sale',
+                previousValue: '$123.99',
+                currentValue: '$99.99',
+              },
+              badge: 'New',
+            }}
+            colorScheme="dark"
+            showCompare
+          />
+          <ProductCard
+            className="w-full"
+            product={{
+              id: '1',
+              href: '#',
+              title: 'Product Name',
+              image: {
+                src: 'https://rstr.in/monogram/vibes/oUL4h2FvqIl',
+                alt: 'Product Name',
+              },
+              subtitle: 'Blue/Black/Green',
+              price: {
+                type: 'range',
+                minValue: '$99.99',
+                maxValue: '$123.99',
+              },
+            }}
+            colorScheme="dark"
+            showCompare
+          />
+        </div>
+      </div>
     </div>
   );
 }
