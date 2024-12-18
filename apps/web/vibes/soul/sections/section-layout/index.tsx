274aba1a618c5e6668accb8ecf760cbd5919b25e
Alan Pledger <apledger3@gmail.com>
2024-12-18 13:19:03 -0500

    feat: add variables to SectionLayout

--- apps/web/vibes/soul/sections/section-layout/index.tsx
+++ apps/web/vibes/soul/sections/section-layout/index.tsx
@@ -1,5 +1,18 @@
 import { clsx } from 'clsx';
 
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --section-max-width-medium: 768px;
+ *   --section-max-width-lg: 1024px;
+ *   --section-max-width-xl: 1280px;
+ *   --section-max-width-2xl: 1536px;
+ * }
+ * ```
+ */
 export function SectionLayout({
   className,
   children,
@@ -7,7 +20,7 @@ export function SectionLayout({
 }: {
   className?: string;
   children: React.ReactNode;
-  containerSize?: 'lg' | 'xl' | '2xl';
+  containerSize?: 'md' | 'lg' | 'xl' | '2xl';
 }) {
   return (
     <section className={clsx('@container', className)}>
@@ -15,9 +28,10 @@ export function SectionLayout({
         className={clsx(
           'mx-auto px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20',
           {
-            lg: 'max-w-screen-lg',
-            xl: 'max-w-screen-xl',
-            '2xl': 'max-w-screen-2xl',
+            md: 'max-w-screen-[var(--section-max-width-md,768px)]',
+            lg: 'max-w-screen-[var(--section-max-width-lg,1024px)]',
+            xl: 'max-w-screen-[var(--section-max-width-xl,1280px)]',
+            '2xl': 'max-w-screen-[var(--section-max-width-2xl,1536px)]',
           }[containerSize],
         )}
       >
