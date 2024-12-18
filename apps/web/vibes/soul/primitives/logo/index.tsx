b3f96132d0194fe5ad5a85b5ab4c08e07150ba41
Alan Pledger <apledger3@gmail.com>
2024-12-18 17:44:05 -0500

    feat: add footer variables and props

---
+++ apps/web/vibes/soul/primitives/logo/index.tsx
@@ -0,0 +1,62 @@
+import { clsx } from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Stream, Streamable } from '@/vibes/soul/lib/streamable';
+
+interface Props {
+  className?: string;
+  logo?: Streamable<string | { src: string; alt: string } | null>;
+  label?: string;
+  href: string;
+  width: number;
+}
+
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --logo-focus: hsl(var(--primary));
+ *   --logo-font-family: var(--font-family-heading);
+ *   --logo-text: hsl(var(--foreground));
+ * }
+ * ```
+ */
+export function Logo({ className, logo: streamableLogo, href, width, label }: Props) {
+  return (
+    <Stream
+      fallback={<div className="h-6 w-16 animate-pulse rounded-md bg-contrast-100" />}
+      value={streamableLogo}
+    >
+      {(logo) => (
+        <Link
+          aria-label={label}
+          className={clsx(
+            'relative h-full items-center outline-0 ring-[var(--logo-focus,hsl(var(--primary)))] ring-offset-4 focus-visible:ring-2',
+            className,
+          )}
+          href={href}
+          style={typeof logo === 'string' ? {} : { width }}
+        >
+          {typeof logo === 'object' && logo !== null && logo.src !== '' ? (
+            <Image
+              alt={logo.alt}
+              className="object-contain object-left"
+              fill
+              sizes="25vw"
+              src={logo.src}
+            />
+          ) : (
+            typeof logo === 'string' && (
+              <span className="font-[family-name:var(--logo-font-family,var(--font-family-heading))] text-lg font-semibold leading-none text-[var(--logo-text,hsl(var(--foreground)))] @xl:text-2xl">
+                {logo}
+              </span>
+            )
+          )}
+        </Link>
+      )}
+    </Stream>
+  );
+}
