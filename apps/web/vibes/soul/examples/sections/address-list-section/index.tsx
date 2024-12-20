d2d4bce9d5bc8689be5b1b943ed25350bef3c7f9
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-12-20 15:53:55 -0500

    feat: migrate address form to use dynamic form

--- apps/web/vibes/soul/examples/sections/address-list-section/index.tsx
+++ apps/web/vibes/soul/examples/sections/address-list-section/index.tsx
@@ -1,3 +1,4 @@
+import { Field, FieldGroup } from '@/vibes/soul/primitives/dynamic-form/schema';
 import { AccountLayout } from '@/vibes/soul/sections/account-layout';
 import { Address, AddressListSection } from '@/vibes/soul/sections/address-list-section';
 
@@ -9,21 +10,21 @@ const addresses: Address[] = [
     firstName: 'Ben',
     lastName: 'Smith',
     company: 'Google',
-    street1: '123 Main St',
+    address1: '123 Main St',
     city: 'San Francisco',
-    state: 'CA',
+    stateOrProvince: 'CA',
     postalCode: '94105',
-    country: 'United States',
+    countryCode: 'United States',
   },
   {
     id: '2',
     firstName: 'Sherlock',
     lastName: 'Holmes',
-    street1: '456 Main St',
+    address1: '456 Main St',
     city: 'San Francisco',
-    state: 'CA',
+    stateOrProvince: 'CA',
     postalCode: '94105',
-    country: 'United States',
+    countryCode: 'United States',
   },
 ];
 
@@ -33,6 +34,79 @@ const links = [
   { href: '/preview/soul/account-settings-section-example', label: 'Account' },
 ];
 
+const fields: Array<Field | FieldGroup<Field>> = [
+  [
+    {
+      type: 'text',
+      name: 'firstName',
+      label: 'First Name',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'lastName',
+      label: 'Last Name',
+      required: true,
+    },
+  ],
+  {
+    type: 'text',
+    name: 'company',
+    label: 'Company Name',
+    required: false,
+  },
+  {
+    type: 'text',
+    name: 'phone',
+    label: 'Phone Number',
+    required: false,
+  },
+  {
+    type: 'text',
+    name: 'street1',
+    label: 'Address Line 1',
+    required: true,
+  },
+  {
+    type: 'text',
+    name: 'street2',
+    label: 'Address Line 2',
+    required: false,
+  },
+  [
+    {
+      type: 'text',
+      name: 'city',
+      label: 'Suburb/City',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'state',
+      label: 'State/Province',
+      required: true,
+    },
+  ],
+  [
+    {
+      type: 'text',
+      name: 'postalCode',
+      label: 'Zip/Postcode',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'country',
+      label: 'Country',
+      required: true,
+    },
+  ],
+  {
+    name: 'id',
+    type: 'hidden',
+  },
+];
+
 export default function Preview() {
   return (
     <AccountLayout links={links}>
@@ -40,6 +114,7 @@ export default function Preview() {
         addressAction={addressAction}
         addresses={addresses}
         defaultAddress={{ id: '1' }}
+        fields={fields}
       />
     </AccountLayout>
   );
