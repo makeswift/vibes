43fe5dfc039766f49cc750c106e52766af5de73e
Alan Pledger <apledger3@gmail.com>
2025-01-02 17:40:48 -0500

    refactor: cart to use Streamable, summary props, skeleton responsiveness

--- apps/web/vibes/soul/examples/sections/cart/electric.tsx
+++ apps/web/vibes/soul/examples/sections/cart/electric.tsx
@@ -1,42 +1,25 @@
-import { getLineItems, getSubtotal } from '@/vibes/soul/data/line-items';
+import { getLineItems } from '@/vibes/soul/data/line-items';
 import { Cart, CartLineItem } from '@/vibes/soul/sections/cart';
 
 import { checkoutAction, lineItemAction } from './actions';
 
 export default async function Preview() {
-  // const products = await getLineItems('Electric')
-  const lineItems = new Promise<CartLineItem[]>((res) =>
-    setTimeout(() => res(getLineItems('Electric')), 5000),
+  const lineItems = await getLineItems('Electric');
+  const cart = new Promise<Cart<CartLineItem>>((res) =>
+    setTimeout(
+      () =>
+        res({
+          lineItems,
+          summaryLineItems: [
+            { label: 'Subtotal', value: '$100' },
+            { label: 'Shipping', value: 'TBD' },
+            { label: 'Tax', value: 'TBD' },
+          ],
+          total: '127.60',
+        }),
+      1000,
+    ),
   );
-  const subtotal = await getSubtotal('Electric');
 
-  return (
-    <Cart
-      checkoutAction={checkoutAction}
-      emptyState={{
-        title: 'Your cart is empty',
-        subtitle: 'Add some products to get started.',
-        cta: {
-          label: 'Continue shopping',
-          href: '#',
-        },
-      }}
-      lineItemAction={lineItemAction}
-      lineItems={lineItems}
-      summary={{
-        title: 'Summary',
-        subtotal: subtotal,
-        caption: 'Shipping & taxes calculated at checkout',
-        subtotalLabel: 'Subtotal',
-        shippingLabel: 'Shipping',
-        shipping: 'TBD',
-        taxLabel: 'Tax',
-        tax: 'TBD',
-        // grandTotalLabel: 'Total',
-        // grandTotal: '$127.60',
-        ctaLabel: 'Checkout',
-      }}
-      title="Cart"
-    />
-  );
+  return <Cart cart={cart} checkoutAction={checkoutAction} lineItemAction={lineItemAction} />;
 }
--- apps/web/vibes/soul/examples/sections/cart/luxury.tsx
+++ apps/web/vibes/soul/examples/sections/cart/luxury.tsx
@@ -1,39 +1,25 @@
-import { getLineItems, getSubtotal } from '@/vibes/soul/data/line-items';
-import { Cart } from '@/vibes/soul/sections/cart';
+import { getLineItems } from '@/vibes/soul/data/line-items';
+import { Cart, CartLineItem } from '@/vibes/soul/sections/cart';
 
 import { checkoutAction, lineItemAction } from './actions';
 
 export default async function Preview() {
   const lineItems = await getLineItems('Luxury');
-  const subtotal = await getSubtotal('Luxury');
-
-  return (
-    <Cart
-      checkoutAction={checkoutAction}
-      emptyState={{
-        title: 'Your cart is empty',
-        subtitle: 'Add some products to get started.',
-        cta: {
-          label: 'Continue shopping',
-          href: '#',
-        },
-      }}
-      lineItemAction={lineItemAction}
-      lineItems={lineItems}
-      summary={{
-        title: 'Summary',
-        subtotal: subtotal,
-        caption: 'Shipping & taxes calculated at checkout',
-        subtotalLabel: 'Subtotal',
-        shippingLabel: 'Shipping',
-        shipping: 'TBD',
-        taxLabel: 'Tax',
-        tax: 'TBD',
-        // grandTotalLabel: 'Total',
-        // grandTotal: '$127.60',
-        ctaLabel: 'Checkout',
-      }}
-      title="Cart"
-    />
+  const cart = new Promise<Cart<CartLineItem>>((res) =>
+    setTimeout(
+      () =>
+        res({
+          lineItems,
+          summaryLineItems: [
+            { label: 'Subtotal', value: '$100' },
+            { label: 'Shipping', value: 'TBD' },
+            { label: 'Tax', value: 'TBD' },
+          ],
+          total: '127.60',
+        }),
+      1000,
+    ),
   );
+
+  return <Cart cart={cart} checkoutAction={checkoutAction} lineItemAction={lineItemAction} />;
 }
