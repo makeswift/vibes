5d888081909c085a96bd28cca6e3dcb91d7005c9
jordanarldt <jordan.arldt@bigcommerce.com>
2025-01-13 11:49:45 -0600

    Refactor SignUpSection to  DynamicFormSection

--- apps/web/vibes/soul/examples/sections/sign-up-section/index.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUpSection } from '@/vibes/soul/sections/sign-up-section';
-
-import { action, fields } from './actions';
-
-export default function Preview() {
-  return <SignUpSection action={action} fields={fields} />;
-}
