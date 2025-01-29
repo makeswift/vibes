632126d17b344d69b9cb7d2f5177c4679a4964b3
jamesqquick <james.q.quick@gmail.com>
2025-01-29 08:34:14 -0600

    chore: static data for electric cart example

--- apps/web/vibes/soul/examples/sections/cart/electric.tsx
+++ apps/web/vibes/soul/examples/sections/cart/electric.tsx
@@ -1,6 +1,45 @@
-import { Cart, CartLineItem } from '@/vibes/soul/sections/cart';
+import { SubmissionResult } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
 
-import { checkoutAction, lineItemAction } from './actions';
+import { Cart, CartLineItem } from '@/vibes/soul/sections/cart';
+import { cartLineItemActionFormDataSchema } from '@/vibes/soul/sections/cart/schema';
+
+
+export default function Preview() {
+  const lineItemsPromise = new Promise<CartLineItem[]>((res) =>
+    setTimeout(() => res(lineItems), 3000),
+  );
+
+  return (
+    <Cart
+      checkoutAction={checkoutAction}
+      emptyState={{
+        title: 'Your cart is empty',
+        subtitle: 'Add some products to get started.',
+        cta: {
+          label: 'Continue shopping',
+          href: '#',
+        },
+      }}
+      lineItemAction={lineItemAction}
+      lineItems={lineItemsPromise}
+      summary={{
+        title: 'Summary',
+        subtotal,
+        caption: 'Shipping & taxes calculated at checkout',
+        subtotalLabel: 'Subtotal',
+        shippingLabel: 'Shipping',
+        shipping: 'TBD',
+        taxLabel: 'Tax',
+        tax: 'TBD',
+        grandTotalLabel: 'Total',
+        grandTotal: '$127.60',
+        ctaLabel: 'Checkout',
+      }}
+      title="Cart"
+    />
+  );
+}
 
 const lineItems: CartLineItem[] = [
   {
@@ -29,39 +68,75 @@ const lineItems: CartLineItem[] = [
 
 const subtotal = `$96`;
 
-export default function Preview() {
-  const lineItemsPromise = new Promise<CartLineItem[]>((res) =>
-    setTimeout(() => res(lineItems), 5000),
-  );
+export async function checkoutAction(
+  prevState: Awaited<SubmissionResult | null>,
+): Promise<SubmissionResult | null> {
+  'use server';
 
-  return (
-    <Cart
-      checkoutAction={checkoutAction}
-      emptyState={{
-        title: 'Your cart is empty',
-        subtitle: 'Add some products to get started.',
-        cta: {
-          label: 'Continue shopping',
-          href: '#',
-        },
-      }}
-      lineItemAction={lineItemAction}
-      lineItems={lineItemsPromise}
-      summary={{
-        title: 'Summary',
-        subtotal,
-        caption: 'Shipping & taxes calculated at checkout',
-        subtotalLabel: 'Subtotal',
-        shippingLabel: 'Shipping',
-        shipping: 'TBD',
-        taxLabel: 'Tax',
-        tax: 'TBD',
-        // grandTotalLabel: 'Total',
-        // grandTotal: '$127.60',
-        ctaLabel: 'Checkout',
-      }}
-      title="Cart"
-    />
-  );
+  // Simulate a network request
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return prevState;
 }
 
+export async function lineItemAction(
+  prevState: Awaited<{
+    lineItems: CartLineItem[];
+    lastResult: SubmissionResult | null;
+  }>,
+  formData: FormData,
+): Promise<{
+  lineItems: CartLineItem[];
+  lastResult: SubmissionResult | null;
+}> {
+  'use server';
+
+  const submission = parseWithZod(formData, { schema: cartLineItemActionFormDataSchema });
+
+  if (submission.status !== 'success') {
+    return {
+      ...prevState,
+      lastResult: submission.reply({ formErrors: ['Boom!'] }),
+    };
+  }
+
+  // Simulate a network request
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  switch (submission.value.intent) {
+    case 'increment': {
+      // const item = await incrementLineItem(submission.value)
+      const item = submission.value;
+
+      return {
+        lineItems: prevState.lineItems.map((lineItem) =>
+          lineItem.id === item.id ? { ...lineItem, quantity: lineItem.quantity + 1 } : lineItem,
+        ),
+        lastResult: submission.reply({ resetForm: true }),
+      };
+    }
+    case 'decrement': {
+      // const item = await decrementLineItem(submission.value)
+      const item = submission.value;
+
+      return {
+        lineItems: prevState.lineItems.map((lineItem) =>
+          lineItem.id === item.id ? { ...lineItem, quantity: lineItem.quantity - 1 } : lineItem,
+        ),
+        lastResult: submission.reply({ resetForm: true }),
+      };
+    }
+    case 'delete': {
+      // const deletedItem = await deleteLineItem(submission.value)
+      const deletedItem = submission.value;
+
+      return {
+        lineItems: prevState.lineItems.filter((item) => item.id !== deletedItem.id),
+        lastResult: submission.reply({ resetForm: true }),
+      };
+    }
+    default: {
+      return prevState;
+    }
+  }
+}
--- apps/web/vibes/soul/examples/sections/cart/warm.tsx
+++ apps/web/vibes/soul/examples/sections/cart/warm.tsx
@@ -1,11 +1,13 @@
-import { getLineItems, getSubtotal } from '@/vibes/soul/data/line-items';
-import { Cart } from '@/vibes/soul/sections/cart';
+import { SubmissionResult } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
 
-import { checkoutAction, lineItemAction } from './actions';
+import { Cart, CartLineItem } from '@/vibes/soul/sections/cart';
+import { cartLineItemActionFormDataSchema } from '@/vibes/soul/sections/cart/schema';
 
-export default async function Preview() {
-  const lineItems = await getLineItems('Warm');
-  const subtotal = await getSubtotal('Warm');
+export default function Preview() {
+  const lineItemsPromise = new Promise<CartLineItem[]>((res) =>
+    setTimeout(() => res(lineItems), 3000),
+  );
 
   return (
     <Cart
@@ -19,7 +21,7 @@ export default async function Preview() {
         },
       }}
       lineItemAction={lineItemAction}
-      lineItems={lineItems}
+      lineItems={lineItemsPromise}
       summary={{
         title: 'Summary',
         subtotal: subtotal,
@@ -36,4 +38,104 @@ export default async function Preview() {
       title="Cart"
     />
   );
+}
+
+const lineItems: CartLineItem[] = [
+  {
+    id: '1',
+    title: 'Rolltop Saddlebag',
+    subtitle: 'Orange',
+    price: '$50',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/4Mo9ulLGcbL/DfL7Hp4ix9B',
+      alt: 'Rolltop Saddlebag',
+    },
+    quantity: 1,
+  },
+  {
+    id: '2',
+    title: 'Mini Bar Bag',
+    subtitle: 'Camo',
+    price: '$60',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/JFeKAqWOECR',
+      alt: 'Mini Bar Bag',
+    },
+    quantity: 2,
+  },
+];
+
+const subtotal = `$170`;
+
+async function checkoutAction(
+  prevState: Awaited<SubmissionResult | null>,
+): Promise<SubmissionResult | null> {
+  'use server';
+
+  // Simulate a network request
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return prevState;
+}
+
+export async function lineItemAction(
+  prevState: Awaited<{
+    lineItems: CartLineItem[];
+    lastResult: SubmissionResult | null;
+  }>,
+  formData: FormData,
+): Promise<{
+  lineItems: CartLineItem[];
+  lastResult: SubmissionResult | null;
+}> {
+  'use server';
+
+  const submission = parseWithZod(formData, { schema: cartLineItemActionFormDataSchema });
+
+  if (submission.status !== 'success') {
+    return {
+      ...prevState,
+      lastResult: submission.reply({ formErrors: ['Boom!'] }),
+    };
+  }
+
+  // Simulate a network request
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  switch (submission.value.intent) {
+    case 'increment': {
+      // const item = await incrementLineItem(submission.value)
+      const item = submission.value;
+
+      return {
+        lineItems: prevState.lineItems.map((lineItem) =>
+          lineItem.id === item.id ? { ...lineItem, quantity: lineItem.quantity + 1 } : lineItem,
+        ),
+        lastResult: submission.reply({ resetForm: true }),
+      };
+    }
+    case 'decrement': {
+      // const item = await decrementLineItem(submission.value)
+      const item = submission.value;
+
+      return {
+        lineItems: prevState.lineItems.map((lineItem) =>
+          lineItem.id === item.id ? { ...lineItem, quantity: lineItem.quantity - 1 } : lineItem,
+        ),
+        lastResult: submission.reply({ resetForm: true }),
+      };
+    }
+    case 'delete': {
+      // const deletedItem = await deleteLineItem(submission.value)
+      const deletedItem = submission.value;
+
+      return {
+        lineItems: prevState.lineItems.filter((item) => item.id !== deletedItem.id),
+        lastResult: submission.reply({ resetForm: true }),
+      };
+    }
+    default: {
+      return prevState;
+    }
+  }
 }
