499c216f35902c9dbce8bb4d944f1ee32fd6a104
jamesqquick <james.q.quick@gmail.com>
2025-01-31 12:58:37 -0600

    chore: inline data for compare section

--- apps/web/vibes/soul/examples/sections/compare-section/warm.tsx
+++ apps/web/vibes/soul/examples/sections/compare-section/warm.tsx
@@ -1,10 +1,12 @@
-import { getProducts } from '@/vibes/soul/data';
 import { CompareSection } from '@/vibes/soul/sections/compare-section';
 
-import { addToCartAction } from './actions';
+import { CompareProduct } from '@/vibes/soul/primitives/compare-card';
 
 export default function Preview() {
-  const products = getProducts('Warm');
+  const products = new Promise<CompareProduct[]>((resolve)  => {
+    setTimeout(() => resolve(defaultProducts), 1000);
+    }
+  );
 
   return (
     <CompareSection
@@ -15,3 +17,86 @@ export default function Preview() {
     />
   );
 }
+
+export async function addToCartAction(id: string) {
+  'use server';
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  console.log('Add to cart:', id);
+}
+
+const defaultProducts: CompareProduct[] =
+[
+  {
+    id: '1',
+    title: 'Mini Bar Bag',
+    subtitle: 'Blue/Black/Green',
+    price: '$65',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/mrlTNE1TJfB',
+      alt: 'Mini Bar Bag',
+    },
+    href: '#',
+    rating: 4.3,
+  },
+  {
+    id: '2',
+    title: 'Mini Bar Bag',
+    subtitle: 'Blue/Black/Green',
+    price: '$65',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/LznMEk1GSB1',
+      alt: 'Mini Bar Bag',
+    },
+    href: '#',
+    rating: 4.5,
+  },
+  {
+    id: '3',
+    title: 'Stem Caddy',
+    subtitle: 'Blue/Black/Green',
+    price: '$60',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/EpL5yspw4Pc',
+      alt: 'Stem Caddy',
+    },
+    href: '#',
+    rating: 4.2,
+  },
+  {
+    id: '4',
+    title: 'Hip Slinger',
+    subtitle: 'Blue/Black/Green',
+    price: '$105',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/z6b0vDjJv6x',
+      alt: 'Hip Slinger',
+    },
+    href: '#',
+    rating: 4.6,
+  },
+  {
+    id: '5',
+    title: 'Everyday Tote',
+    subtitle: 'Blue/Black/Green',
+    price: '$185',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/1tVm6tBbJq9',
+      alt: 'Everyday Tote',
+    },
+    href: '#',
+    rating: 4.8,
+  },
+  {
+    id: '6',
+    title: 'Mini Saddlebag',
+    subtitle: 'Blue/Black/Green',
+    price: '$45',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/MZX8-yya26e',
+      alt: 'Mini Saddlebag',
+    },
+    href: '#',
+    rating: 4.1,
+  },
+]
