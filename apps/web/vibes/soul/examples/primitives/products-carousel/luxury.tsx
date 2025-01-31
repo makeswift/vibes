2a057497100801aad80843982ee296cb31ca9ece
jamesqquick <james.q.quick@gmail.com>
2025-01-31 11:18:44 -0600

    chore: inline data for product primitive examples and shorten names

--- apps/web/vibes/soul/examples/primitives/products-carousel/luxury.tsx
+++ apps/web/vibes/soul/examples/primitives/products-carousel/luxury.tsx
@@ -1,4 +1,4 @@
-import { ProductsList } from '@/vibes/soul/primitives/products-list';
+import { ProductsCarousel } from '@/vibes/soul/primitives/products-carousel';
 
 export default function Preview() {
   const products = new Promise<Product[]>((resolve)  => {
@@ -7,8 +7,17 @@ export default function Preview() {
   );
 
   return (
-    <div className="p-6">
-      <ProductsList products={products} />
+    <div>
+      <section className="overflow-hidden @container">
+        <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
+          <ProductsCarousel products={products} />
+        </div>
+      </section>
+      <section className="overflow-hidden bg-foreground @container">
+        <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
+          <ProductsCarousel colorScheme="dark" products={products} showScrollbar={false} />
+        </div>
+      </section>
     </div>
   );
 }
@@ -26,7 +35,6 @@ interface Product {
   href: string;
   rating: number;
 }
-
 
 const defaultProducts: Product[] = [
   {
