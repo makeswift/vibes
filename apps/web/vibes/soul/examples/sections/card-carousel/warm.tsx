b1968ca273415a79492452beaaaf629e35716f13
jamesqquick <james.q.quick@gmail.com>
2025-02-21 15:34:30 -0600

    docs: add docs for card carousel

--- apps/web/vibes/soul/examples/sections/card-carousel/warm.tsx
+++ apps/web/vibes/soul/examples/sections/card-carousel/warm.tsx
@@ -1,15 +1,19 @@
-import { getProducts } from '@/vibes/soul/data';
+import { CardWithId } from '@/vibes/soul/primitives/card';
 import { CardCarousel } from '@/vibes/soul/sections/card-carousel';
 
 export default function Preview() {
-  const cards = getProducts('Warm');
+  const cardsPromise = new Promise<CardWithId[]>((resolve) => {
+    setTimeout(() => {
+      resolve(cards);
+    }, 1000);
+  });
 
   return (
     <div>
       <section className="group/pending overflow-hidden @container">
         <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 group-has-[[data-pending]]/pending:animate-pulse @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
           <CardCarousel
-            cards={cards}
+            cards={cardsPromise}
             className="w-full"
             emptyStateSubtitle="Try browsing our complete catalog of products."
             emptyStateTitle="No products found"
@@ -20,7 +24,7 @@ export default function Preview() {
       <section className="group/pending overflow-hidden bg-foreground @container">
         <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 group-has-[[data-pending]]/pending:animate-pulse @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
           <CardCarousel
-            cards={cards}
+            cards={cardsPromise}
             className="w-full"
             emptyStateSubtitle="Try browsing our complete catalog of products."
             emptyStateTitle="No products found"
@@ -32,3 +36,60 @@ export default function Preview() {
     </div>
   );
 }
+
+const cards: CardWithId[] = [
+  {
+    id: '1',
+    title: 'Mini Bar Bag',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/mrlTNE1TJfB',
+      alt: 'Mini Bar Bag',
+    },
+    href: '#',
+  },
+  {
+    id: '2',
+    title: 'Mini Bar Bag',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/LznMEk1GSB1',
+      alt: 'Mini Bar Bag',
+    },
+    href: '#',
+  },
+  {
+    id: '3',
+    title: 'Stem Caddy',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/EpL5yspw4Pc',
+      alt: 'Stem Caddy',
+    },
+    href: '#',
+  },
+  {
+    id: '4',
+    title: 'Hip Slinger',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/z6b0vDjJv6x',
+      alt: 'Hip Slinger',
+    },
+    href: '#',
+  },
+  {
+    id: '5',
+    title: 'Everyday Tote',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/1tVm6tBbJq9',
+      alt: 'Everyday Tote',
+    },
+    href: '#',
+  },
+  {
+    id: '6',
+    title: 'Mini Saddlebag',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/MZX8-yya26e',
+      alt: 'Mini Saddlebag',
+    },
+    href: '#',
+  },
+];
