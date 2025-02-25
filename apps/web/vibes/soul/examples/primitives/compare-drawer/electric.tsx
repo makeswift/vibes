b6413ee129289361f2d0cb9d794c6323f52ef8ad
Hunter Garrett <hunter@hunterbecton.com>
2025-02-25 18:05:30 -0500

    audit: compare drawer

---
+++ apps/web/vibes/soul/examples/primitives/compare-drawer/electric.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { CompareDrawer } from '@/vibes/soul/primitives/compare-drawer';
+
+export default function Preview() {
+  return (
+    <div className="relative h-screen">
+      <CompareDrawer items={compareProducts} />
+    </div>
+  );
+}
+
+const compareProducts = [
+  {
+    id: '1',
+    title: 'Philodendron Imperial Red',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/-kv08IvX08j',
+      alt: 'Philodendron Imperial Red',
+    },
+    href: '#',
+  },
+  {
+    id: '2',
+    title: 'Monstera',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/n0P83RMnClS',
+      alt: 'Monstera',
+    },
+    href: '#',
+  },
+  {
+    id: '3',
+    title: 'Pink Caladium',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/AaZW4j2VTd4',
+      alt: 'Pink Caladium',
+    },
+    href: '#',
+  },
+  {
+    id: '4',
+    title: 'Hoya Kerrii',
+    image: {
+      src: 'https://rstr.in/monogram/vibes/QSaMw6aC_AN',
+      alt: 'Hoya Kerrii',
+    },
+    href: '#',
+  },
+];
