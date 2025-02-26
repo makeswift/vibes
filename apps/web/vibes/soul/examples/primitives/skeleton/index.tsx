f261c502cf6093b89172096d730f33cade8368af
jamesqquick <james.q.quick@gmail.com>
2025-02-26 13:11:47 -0600

    docs: update example to remove lucide icons

--- apps/web/vibes/soul/examples/primitives/skeleton/index.tsx
+++ apps/web/vibes/soul/examples/primitives/skeleton/index.tsx
@@ -1,5 +1,3 @@
-import { ArrowLeft, ArrowRight } from 'lucide-react';
-
 import * as Skeleton from '@/vibes/soul/primitives/skeleton';
 
 export default function Preview() {
@@ -9,10 +7,6 @@ export default function Preview() {
         <Skeleton.Box className="h-32 w-full rounded-xl" />
         <Skeleton.Text characterCount={24} className="w-full rounded text-2xl" />
         <Skeleton.Text characterCount={16} className="w-full rounded text-2xl" />
-        <div className="mt-10 flex justify-between self-center">
-          <Skeleton.Icon icon={<ArrowLeft className="h-6 w-6" strokeWidth={1.5} />} />
-          <Skeleton.Icon icon={<ArrowRight className="h-6 w-6" strokeWidth={1.5} />} />
-        </div>
       </Skeleton.Root>
     </div>
   );
