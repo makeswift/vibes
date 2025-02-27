0f6eb31efa53cccacd43dade5179fa418c5716b5
Hunter Garrett <hunter@hunterbecton.com>
2025-02-27 12:15:24 -0500

    audit: modal

--- apps/web/vibes/soul/primitives/modal/index.tsx
+++ apps/web/vibes/soul/primitives/modal/index.tsx
@@ -1,29 +1,43 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { clsx } from 'clsx';
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-interface Props {
+export interface ModalProps {
   isOpen: boolean;
   setOpen: (open: boolean) => void;
+  title: string;
   trigger: ReactNode;
-  content: ReactNode;
+  children: ReactNode;
 }
 
-export const Modal = function Modal({ isOpen, setOpen, trigger, content }: Props) {
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --modal-background: hsl(var(--background));
+ *   --modal-overlay-background: hsl(var(--foreground)/50%);
+ * }
+ * ```
+ */
+export const Modal = function Modal({ isOpen, setOpen, title, trigger, children }: ModalProps) {
   return (
     <Dialog.Root onOpenChange={setOpen} open={isOpen}>
       <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 z-30 flex items-center justify-center bg-foreground/50 @container">
+        <Dialog.Overlay className="fixed inset-0 z-30 flex items-center justify-center bg-[var(--modal-overlay-background,hsl(var(--foreground)/50%))] @container">
           <Dialog.Content
             className={clsx(
-              'mx-3 my-10 max-h-[90%] max-w-3xl overflow-y-auto rounded-xl bg-background px-3 pb-5 pt-5 @sm:px-6 @sm:pb-10 @sm:pt-8 @5xl:px-20 @5xl:pb-12 @5xl:pt-10',
+              'mx-3 my-10 max-h-[90%] max-w-3xl overflow-y-auto rounded-xl bg-[var(--modal-background,hsl(var(--background)))] px-3 py-5 @sm:px-6 @sm:py-8 @5xl:px-20 @5xl:py-10',
               'transition ease-out',
               'data-[state=closed]:duration-200 data-[state=open]:duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out',
-              'data-[state=closed]:slide-out-to-bottom-16 data-[state=open]:slide-in-from-bottom-16',
+              'focus:outline-none data-[state=closed]:slide-out-to-bottom-16 data-[state=open]:slide-in-from-bottom-16',
             )}
+            onOpenAutoFocus={(e) => e.preventDefault()}
           >
-            {content}
+            <Dialog.Title className="sr-only">{title}</Dialog.Title>
+            {children}
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
