f2eb8f5a561a406eed378f0dc3e68db8dd42acda
Hunter Garrett <hunter@hunterbecton.com>
2025-02-28 15:05:38 -0500

    audit: toaster

--- apps/web/vibes/soul/examples/primitives/toaster/index.tsx
+++ apps/web/vibes/soul/examples/primitives/toaster/index.tsx
@@ -5,34 +5,32 @@ import { toast } from '@/vibes/soul/primitives/toaster';
 
 export default function Preview() {
   return (
-    <div className="grid h-screen place-content-center gap-x-4 gap-y-6">
-      <Button onClick={() => toast.success('Success')} variant="primary">
-        Success
-      </Button>
-
-      <Button onClick={() => toast.error('Error')} variant="primary">
-        Error
-      </Button>
-
-      <Button onClick={() => toast.warning('Warning')} variant="primary">
-        Warning
-      </Button>
-
-      <Button onClick={() => toast.info('Info')} variant="primary">
-        Info
-      </Button>
-
-      <Button
-        onClick={() =>
-          toast.success('Success', {
-            description: 'Description of toast',
-            action: { label: 'Undo', onClick: () => console.log('undo') },
-          })
-        }
-        variant="primary"
-      >
-        Options
-      </Button>
+    <div className="bg-background p-8 @container">
+      <div className="mx-auto flex max-w-screen-md flex-col items-center gap-8">
+        <Button onClick={() => toast.success('Success')} variant="primary">
+          Success
+        </Button>
+        <Button onClick={() => toast.error('Error')} variant="primary">
+          Error
+        </Button>
+        <Button onClick={() => toast.warning('Warning')} variant="primary">
+          Warning
+        </Button>
+        <Button onClick={() => toast.info('Info')} variant="primary">
+          Info
+        </Button>
+        <Button
+          onClick={() =>
+            toast.success('Success', {
+              description: 'Description of toast',
+              action: { label: 'Undo', onClick: () => console.log('undo') },
+            })
+          }
+          variant="primary"
+        >
+          Options
+        </Button>
+      </div>
     </div>
   );
 }
