94cebe84efd4b3ea2d3736243ce8fd7d3a0d5926
jamesqquick <james.q.quick@gmail.com>
2025-02-28 12:08:30 -0600

    docs: price label

--- apps/web/vibes/soul/examples/primitives/price-label/index.tsx
+++ apps/web/vibes/soul/examples/primitives/price-label/index.tsx
@@ -2,7 +2,7 @@ import { PriceLabel } from '@/vibes/soul/primitives/price-label';
 
 export default function Preview() {
   return (
-    <div className="flex h-screen items-center justify-center gap-4">
+    <div className="flex h-screen items-center justify-center gap-16">
       <PriceLabel price="100" />
       <PriceLabel price={{ type: 'sale', previousValue: '150', currentValue: '100' }} />
       <PriceLabel price={{ type: 'range', minValue: '100', maxValue: '200' }} />
