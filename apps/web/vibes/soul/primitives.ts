d787c67fa54467baf541d93cb78d679eb4871540
jamesqquick <james.q.quick@gmail.com>
2025-03-04 13:24:25 -0600

    docs: counter docs

--- apps/web/vibes/soul/primitives.ts
+++ apps/web/vibes/soul/primitives.ts
@@ -105,7 +105,7 @@ export const primitives = [
   },
   {
     name: 'counter',
-    dependencies: ['lucide-react'],
+    dependencies: ['lucide-react', 'clsx'],
     registryDependencies: [],
     files: ['primitives/counter/index.tsx'],
   },
