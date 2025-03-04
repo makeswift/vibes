88b1b05da0623d5ff3aba55c2678ec775146e8e2
jamesqquick <james.q.quick@gmail.com>
2025-03-04 13:40:07 -0600

    docs: inline email form docs

--- apps/web/vibes/soul/examples/sections/inline-email-form/index.tsx
+++ apps/web/vibes/soul/examples/sections/inline-email-form/index.tsx
@@ -1,11 +1,28 @@
 import { InlineEmailForm } from '@/vibes/soul/sections/inline-email-form';
 
-import { action } from './actions';
+import { parseWithZod } from '@conform-to/zod';
+import { z } from 'zod';
 
 export default function Preview() {
   return (
     <div className="p-10">
-      <InlineEmailForm action={action} />
+      <InlineEmailForm
+        action={async (lastResult, formData) => {
+          'use server';
+
+          const schema = z.object({
+            email: z.string().email(),
+          });
+
+          const submission = parseWithZod(formData, { schema });
+
+          if (submission.status !== 'success') {
+            return { lastResult: submission.reply({ formErrors: ['Boom!'] }) };
+          }
+
+          return { lastResult: submission.reply(), successMessage: 'Subscribed!' };
+        }}
+      />{' '}
     </div>
   );
 }
