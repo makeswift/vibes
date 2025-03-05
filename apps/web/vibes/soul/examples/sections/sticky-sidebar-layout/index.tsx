a1d3c5b5a44ce0b4ffd3a44c331873bd8409d0ab
Hunter Garrett <hunter@hunterbecton.com>
2025-03-05 14:46:40 -0500

    audit: sidebar layout

--- apps/web/vibes/soul/examples/sections/sticky-sidebar-layout/index.tsx
+++ apps/web/vibes/soul/examples/sections/sticky-sidebar-layout/index.tsx
@@ -1,16 +1,26 @@
 import { SidebarMenu } from '@/vibes/soul/sections/sticky-sidebar-layout/sidebar-menu';
 import { StickySidebarLayout } from '@/vibes/soul/sections/sticky-sidebar-layout';
 
-const links = [
-  { href: '/preview/soul/sticky-sidebar-layout-example/Electric#a', label: 'Orders' },
-  { href: '/preview/soul/sticky-sidebar-layout-example/Electric#b', label: 'Addresses' },
-  { href: '/preview/soul/sticky-sidebar-layout-example/Electric#c', label: 'Account' },
-];
-
 export default function Preview() {
   return (
-    <StickySidebarLayout sidebar={<SidebarMenu links={links} />} sidebarSize="small">
-      Put some stuff here!
+    <StickySidebarLayout sidebar={<SidebarMenu links={links} />} sidebarSize="sm">
+      <div className="relative h-96 overflow-hidden rounded-2xl border border-dashed border-contrast-200">
+        <div className="absolute inset-0 bg-[linear-gradient(45deg,transparent_42.86%,#d1d1d150_42.86%,#d1d1d150_50%,transparent_50%,transparent_92.86%,#d1d1d150_92.86%,#d1d1d150_100%)] bg-[size:9.90px_9.90px]" />
+      </div>
     </StickySidebarLayout>
   );
 }
+
+const links = [
+  { id: 'orders', href: '/preview/soul/sticky-sidebar-layout-example/Electric', label: 'Orders' },
+  {
+    id: 'addresses',
+    href: '/preview/soul/sticky-sidebar-layout-example/Electric#addresses',
+    label: 'Addresses',
+  },
+  {
+    id: 'account',
+    href: '/preview/soul/sticky-sidebar-layout-example/Electric#account',
+    label: 'Account',
+  },
+];
