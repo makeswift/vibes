0a8be284f596a3cd94456128e986cb639e12c0a0
jamesqquick <james.q.quick@gmail.com>
2025-03-05 15:41:14 -0600

    feat: add options for each docs action in plop command

--- plopfile.mjs
+++ plopfile.mjs
@@ -22,40 +22,62 @@ export default function (
         message: "What type of component is this? (pprimitive, section, form)",
         choices: ["primitives", "sections", "form"],
       },
+      {
+        type: "confirm",
+        name: "generateDocFile",
+        message: "Do you want to generate a doc file?",
+      },
+      {
+        type: "confirm",
+        name: "generateExampleFiles",
+        message: "Do you want to generate example files?",
+      },
+      {
+        type: "confirm",
+        name: "registerExampleFiles",
+        message: "Do you want to register the example files?",
+      },
     ],
     actions: function (data) {
       const actions = [];
+
       //action to create the doc file
-      actions.push({
-        type: "add",
-        path: "apps/web/vibes/soul/docs/{{hyphenCase name}}.mdx",
-        templateFile: "plop-templates/docs.mdx.hbs",
-      });
+      if (data.generateDocFile) {
+        actions.push({
+          type: "add",
+          path: "apps/web/vibes/soul/docs/{{hyphenCase name}}.mdx",
+          templateFile: "plop-templates/docs.mdx.hbs",
+        });
+      }
 
       //action to create the example files
-      actions.push({
-        type: "addMany",
-        destination:
-          "apps/web/vibes/soul/examples/{{type}}/{{hyphenCase name}}",
-        templateFiles: [
-          "plop-templates/warm.tsx.hbs",
-          "plop-templates/electric.tsx.hbs",
-          "plop-templates/luxury.tsx.hbs",
-        ],
-      });
+      if (data.generateExampleFiles) {
+        actions.push({
+          type: "addMany",
+          destination:
+            "apps/web/vibes/soul/examples/{{type}}/{{hyphenCase name}}",
+          templateFiles: [
+            "plop-templates/warm.tsx.hbs",
+            "plop-templates/electric.tsx.hbs",
+            "plop-templates/luxury.tsx.hbs",
+          ],
+        });
+      }
 
       //actions to register the different examples
-      const themes = ["warm", "electric", "luxury"];
-      const themeExampleActions = themes.map((theme) => {
-        return {
-          type: "modify",
-          path: "apps/web/vibes/soul/examples.ts",
-          pattern: /\/\/ PLOP: Append new component here/g,
-          templateFile: "plop-templates/register-example.ts.hbs",
-          data: { ...data, theme },
-        };
-      });
-      actions.push(...themeExampleActions);
+      if (data.registerExampleFiles) {
+        const themes = ["warm", "electric", "luxury"];
+        const themeExampleActions = themes.map((theme) => {
+          return {
+            type: "modify",
+            path: "apps/web/vibes/soul/examples.ts",
+            pattern: /\/\/ PLOP: Append new component here/g,
+            templateFile: "plop-templates/register-example.ts.hbs",
+            data: { ...data, theme },
+          };
+        });
+        actions.push(...themeExampleActions);
+      }
 
       return actions;
     },
