96174ebc843b653800376db8fcd39ab9e52f7a07
Hunter Garrett <hunter@hunterbecton.com>
2025-03-09 11:47:17 -0400

    audit: featured image

--- apps/web/vibes/soul/sections/featured-image/index.tsx
+++ apps/web/vibes/soul/sections/featured-image/index.tsx
@@ -1,8 +1,23 @@
 import { clsx } from 'clsx';
 import Image from 'next/image';
 
-import { ButtonLink } from '../../primitives/button-link';
+import { ButtonLink } from '@/vibes/soul/primitives/button-link';
 
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --featured-image-background: color-mix(in oklab, hsl(var(--primary)), black 75%);
+ *   --featured-image-image-background: hsl(var(--primary) / 10%);
+ *   --featured-image-title-font-family: var(--font-family-heading);
+ *   --featured-image-font-family: var(--font-family-body);
+ *   --featured-image-title: hsl(var(--background));
+ *   --featured-image-description: hsl(var(--background));
+ * }
+ * ```
+ */
 export interface FeaturedImageProps {
   title: string;
   description: string;
@@ -18,7 +33,7 @@ export interface FeaturedImageProps {
   mediaAlign?: 'left' | 'right' | 'full';
 }
 
-export const FeaturedImage = function FeaturedImage({
+export function FeaturedImage({
   title,
   description,
   image,
@@ -28,21 +43,19 @@ export const FeaturedImage = function FeaturedImage({
   return (
     <section
       className={clsx(
-        'relative bg-primary-shadow @container',
+        'relative bg-[var(--featured-image-background,color-mix(in_oklab,hsl(var(--primary)),black_75%))] @container',
         mediaAlign === 'full' && 'h-dvh max-h-[700px]',
       )}
     >
       <div className="mx-auto flex h-full max-w-screen-2xl flex-col @3xl:flex-row">
         <div
           className={clsx(
-            'w-full object-cover',
+            'w-full bg-[var(--featured-image-image-background,hsl(var(--primary)/10%))] object-cover',
             mediaAlign === 'full'
               ? 'absolute inset-0 h-full'
               : 'relative aspect-square bg-primary/10 @xl:aspect-[9/6] @3xl:h-dvh @3xl:max-h-[880px] @3xl:w-1/2 @5xl:w-3/5',
-            {
-              '@3xl:order-2 @7xl:mr-20': mediaAlign === 'right',
-              '@7xl:ml-20': mediaAlign === 'left',
-            },
+            mediaAlign === 'right' && '@3xl:order-2 @7xl:mr-20',
+            mediaAlign === 'left' && '@7xl:ml-20',
           )}
         >
           <Image
@@ -55,26 +68,30 @@ export const FeaturedImage = function FeaturedImage({
             src={image.src}
           />
         </div>
-        <div
+        <header
           className={clsx(
-            'z-10 mx-auto flex flex-col items-start gap-4 px-3 pb-20 pt-10 text-background @5xl:p-20',
+            'z-10 mx-auto flex flex-col items-start gap-4 px-3 pb-20 pt-10 font-[family-name:var(--featured-image-font-family,var(--font-family-body))] text-background @5xl:p-20',
             mediaAlign === 'full'
-              ? '5xl:px-20 mx-auto mt-auto w-full max-w-screen-2xl px-3 @xl:px-6'
+              ? 'mx-auto mt-auto w-full max-w-screen-2xl px-3 @5xl:px-20'
               : 'w-full justify-end @xl:px-6 @3xl:w-1/2 @5xl:w-2/5',
-            { '@3xl:order-1': mediaAlign === 'right' },
+            mediaAlign === 'right' && '@3xl:order-1',
           )}
         >
-          <h2 className="max-w-xl font-heading text-4xl leading-none @xl:text-5xl">{title}</h2>
-          <p className="max-w-md pb-2">{description}</p>
+          <h2 className="max-w-xl font-[family-name:var(--featured-image-title-font-family,var(--font-family-heading))] text-4xl leading-none text-[var(--featured-image-title,hsl(var(--background)))] @xl:text-5xl">
+            {title}
+          </h2>
+          <p className="max-w-md pb-2 text-[var(--featured-image-description,hsl(var(--background)))]">
+            {description}
+          </p>
           <ButtonLink
-            className={clsx(mediaAlign === 'full' ? 'text-background' : 'text-foreground')}
+            className={clsx(mediaAlign !== 'full' && 'text-foreground')}
             href={cta.href}
             variant={mediaAlign === 'full' ? 'tertiary' : 'primary'}
           >
             {cta.label}
           </ButtonLink>
-        </div>
+        </header>
       </div>
     </section>
   );
-};
+}
