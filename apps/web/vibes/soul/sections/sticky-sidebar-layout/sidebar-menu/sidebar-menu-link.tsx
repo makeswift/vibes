f8093cdacf2965b2f9e7f4638f1640af06dc0e99
jamesqquick <james.q.quick@gmail.com>
2025-03-13 14:16:51 -0500

    feat: move sidebar menu into sticky sidebar layout

---
+++ apps/web/vibes/soul/sections/sticky-sidebar-layout/sidebar-menu/sidebar-menu-link.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { clsx } from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+export function SidebarMenuLink({
+  className,
+  href,
+  ...rest
+}: React.ComponentPropsWithoutRef<typeof Link>) {
+  const pathname = usePathname();
+  const linkPathname = typeof href === 'string' ? href : (href.pathname ?? null);
+
+  return (
+    <Link
+      {...rest}
+      className={clsx(
+        'flex min-h-10 items-center rounded-md px-3 text-sm font-semibold',
+        linkPathname !== null && pathname.includes(linkPathname)
+          ? 'bg-contrast-100'
+          : 'hover:bg-contrast-100',
+        className,
+      )}
+      href={href}
+    />
+  );
+}
