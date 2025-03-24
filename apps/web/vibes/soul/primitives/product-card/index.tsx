cf64ae0905e5cd2e6f69b78b11449024e79227dd
Hunter Garrett <hunter@hunterbecton.com>
2025-03-24 13:57:52 -0400

    refactor: product-card

--- apps/web/vibes/soul/primitives/product-card/index.tsx
+++ apps/web/vibes/soul/primitives/product-card/index.tsx
@@ -28,7 +28,6 @@ export interface ProductCardProps {
   imageSizes?: string;
   compareLabel?: string;
   compareParamName?: string;
-  borderRadius?: 'none' | 'small' | 'medium' | 'large';
   product: Product;
 }
 
@@ -60,7 +59,6 @@ export function ProductCard({
   compareLabel,
   compareParamName,
   imagePriority = false,
-  borderRadius = 'large',
   imageSizes = '(min-width: 80rem) 20vw, (min-width: 64rem) 25vw, (min-width: 42rem) 33vw, (min-width: 24rem) 50vw, 100vw',
 }: ProductCardProps) {
   return (
@@ -71,64 +69,73 @@ export function ProductCard({
       )}
     >
       <div className="relative">
-        <div
-          className={clsx(
-            'relative overflow-hidden rounded-xl @md:rounded-2xl',
-            {
-              '5:6': 'aspect-[5/6]',
-              '3:4': 'aspect-[3/4]',
-              '1:1': 'aspect-square',
-            }[aspectRatio],
-            {
-              large: 'rounded-xl @md:rounded-2xl',
-              medium: 'rounded-lg @md:rounded-xl',
-              small: 'rounded-md @md:rounded-lg',
-              none: 'rounded-none',
-            }[borderRadius],
-            {
-              light: 'bg-[var(--product-card-light-background,hsl(var(--contrast-100)))]',
-              dark: 'bg-[var(--product-card-dark-background,hsl(var(--contrast-500)))]',
-            }[colorScheme],
-          )}
-        >
-          {image != null ? (
-            <Image
-              alt={image.alt}
-              className={clsx(
-                'w-full scale-100 select-none object-cover transition-transform duration-500 ease-out group-hover:scale-110',
-                {
-                  light: 'bg-[var(--product-card-light-background,hsl(var(--contrast-100))]',
-                  dark: 'bg-[var(--product-card-dark-background,hsl(var(--contrast-500))]',
-                }[colorScheme],
-              )}
-              fill
-              priority={imagePriority}
-              sizes={imageSizes}
-              src={image.src}
-            />
-          ) : (
-            <div
-              className={clsx(
-                'break-words pl-5 pt-5 text-4xl font-bold leading-[0.8] tracking-tighter opacity-25 transition-transform duration-500 ease-out group-hover:scale-105 @xs:text-7xl',
-                {
-                  light: 'text-[var(--product-card-light-title,hsl(var(--foreground)))]',
-                  dark: 'text-[var(--product-card-dark-title,hsl(var(--background)))]',
-                }[colorScheme],
-              )}
-            >
-              {title}
-            </div>
-          )}
-          {badge != null && badge !== '' && (
-            <Badge className="absolute left-3 top-3" shape="rounded">
-              {badge}
-            </Badge>
-          )}
+        <div className="relative">
+          <div
+            className={clsx(
+              'relative overflow-hidden rounded-xl @md:rounded-2xl',
+              {
+                '5:6': 'aspect-[5/6]',
+                '3:4': 'aspect-[3/4]',
+                '1:1': 'aspect-square',
+              }[aspectRatio],
+              {
+                light: 'bg-[var(--product-card-light-background,hsl(var(--contrast-100)))]',
+                dark: 'bg-[var(--product-card-dark-background,hsl(var(--contrast-500)))]',
+              }[colorScheme],
+            )}
+          >
+            {image != null ? (
+              <Image
+                alt={image.alt}
+                className={clsx(
+                  'w-full scale-100 select-none object-cover transition-transform duration-500 ease-out group-hover:scale-110',
+                  {
+                    light: 'bg-[var(--product-card-light-background,hsl(var(--contrast-100))]',
+                    dark: 'bg-[var(--product-card-dark-background,hsl(var(--contrast-500))]',
+                  }[colorScheme],
+                )}
+                fill
+                priority={imagePriority}
+                sizes={imageSizes}
+                src={image.src}
+              />
+            ) : (
+              <div
+                className={clsx(
+                  'break-words pl-5 pt-5 text-4xl font-bold leading-[0.8] tracking-tighter opacity-25 transition-transform duration-500 ease-out group-hover:scale-105 @xs:text-7xl',
+                  {
+                    light: 'text-[var(--product-card-light-title,hsl(var(--foreground)))]',
+                    dark: 'text-[var(--product-card-dark-title,hsl(var(--background)))]',
+                  }[colorScheme],
+                )}
+              >
+                {title}
+              </div>
+            )}
+            {badge != null && badge !== '' && (
+              <Badge className="absolute left-3 top-3" shape="rounded">
+                {badge}
+              </Badge>
+            )}
+          </div>
+          <Link
+            aria-label={title}
+            className={clsx(
+              'absolute inset-0 rounded-xl focus:outline-none focus-visible:ring-2 focus-visible:ring-[var(--product-card-focus,hsl(var(--primary)))] focus-visible:ring-offset-4 @md:rounded-2xl',
+              {
+                light: 'ring-offset-[var(--product-card-light-offset,hsl(var(--background)))]',
+                dark: 'ring-offset-[var(--product-card-dark-offset,hsl(var(--foreground)))]',
+              }[colorScheme],
+            )}
+            href={href}
+            id={id}
+          >
+            <span className="sr-only">View product</span>
+          </Link>
         </div>
-
-        <div className="mt-2 flex flex-col items-start gap-x-4 gap-y-3 px-1 @xs:mt-3 @2xl:flex-row">
-          <div className="flex-1 text-sm @[16rem]:text-base">
-            <span
+        <div className="mt-2 flex flex-col items-start gap-x-4 gap-y-3 px-1 @xs:mt-3 @xs:flex-row">
+          <div className="relative flex-1 text-sm @[16rem]:text-base">
+            <h3
               className={clsx(
                 'block font-semibold',
                 {
@@ -138,12 +145,12 @@ export function ProductCard({
               )}
             >
               {title}
-            </span>
+            </h3>
 
             {subtitle != null && subtitle !== '' && (
               <span
                 className={clsx(
-                  'mb-2 block text-sm font-normal',
+                  'block text-sm font-normal',
                   {
                     light: 'text-[var(--product-card-light-subtitle,hsl(var(--foreground)/75%))]',
                     dark: 'text-[var(--product-card-dark-subtitle,hsl(var(--background)/75%))]',
@@ -153,40 +160,37 @@ export function ProductCard({
                 {subtitle}
               </span>
             )}
-            {price != null && <PriceLabel colorScheme={colorScheme} price={price} />}
+            {price != null && (
+              <PriceLabel className="mt-2" colorScheme={colorScheme} price={price} />
+            )}
+            <Link
+              aria-label={title}
+              className={clsx(
+                'absolute inset-0 rounded focus:outline-none focus-visible:ring-2 focus-visible:ring-[var(--product-card-focus,hsl(var(--primary)))] focus-visible:ring-offset-4',
+                {
+                  light: 'ring-offset-[var(--product-card-light-offset,hsl(var(--background)))]',
+                  dark: 'ring-offset-[var(--product-card-dark-offset,hsl(var(--foreground)))]',
+                }[colorScheme],
+              )}
+              href={href}
+              id={id}
+              tabIndex={-1}
+            >
+              <span className="sr-only">View product</span>
+            </Link>
           </div>
-        </div>
-        <Link
-          aria-label={title}
-          className={clsx(
-            'absolute inset-0 focus:outline-none focus-visible:ring-2 focus-visible:ring-[var(--product-card-focus,hsl(var(--primary)))] focus-visible:ring-offset-4',
-            {
-              light: 'ring-offset-[var(--product-card-light-offset,hsl(var(--background)))]',
-              dark: 'ring-offset-[var(--product-card-dark-offset,hsl(var(--foreground)))]',
-            }[colorScheme],
-            {
-              large: 'rounded-2xl',
-              medium: 'rounded-xl',
-              small: 'rounded-lg',
-              none: 'rounded-none',
-            }[borderRadius],
+          {showCompare && (
+            <div className="shrink-0">
+              <Compare
+                colorScheme={colorScheme}
+                label={compareLabel}
+                paramName={compareParamName}
+                product={{ id, title, href, image }}
+              />
+            </div>
           )}
-          href={href}
-          id={id}
-        >
-          <span className="sr-only">View product</span>
-        </Link>
-      </div>
-      {showCompare && (
-        <div className="mt-0.5 shrink-0">
-          <Compare
-            colorScheme={colorScheme}
-            label={compareLabel}
-            paramName={compareParamName}
-            product={{ id, title, href, image }}
-          />
         </div>
-      )}
+      </div>
     </article>
   );
 }
@@ -194,10 +198,9 @@ export function ProductCard({
 export function ProductCardSkeleton({
   className,
   aspectRatio = '5:6',
-  borderRadius = 'large',
-}: Pick<ProductCardProps, 'aspectRatio' | 'className' | 'borderRadius'>) {
+}: Pick<ProductCardProps, 'className' | 'aspectRatio'>) {
   return (
-    <div className={clsx('@container', className)}>
+    <Skeleton.Root className={clsx(className)}>
       <Skeleton.Box
         className={clsx(
           'rounded-xl @md:rounded-2xl',
@@ -206,21 +209,15 @@ export function ProductCardSkeleton({
             '3:4': 'aspect-[3/4]',
             '1:1': 'aspect-square',
           }[aspectRatio],
-          {
-            large: 'rounded-xl @md:rounded-2xl',
-            medium: 'rounded-lg @md:rounded-xl',
-            small: 'rounded-md @md:rounded-lg',
-            none: 'rounded-none',
-          }[borderRadius],
         )}
       />
-      <div className="mt-2 flex flex-col items-start gap-x-4 gap-y-3 px-1 @xs:mt-3 @2xl:flex-row">
-        <div className="w-full text-sm @[16rem]:text-base">
+      <div className="mt-2 flex flex-col items-start gap-x-4 gap-y-3 px-1 @xs:mt-3 @xs:flex-row">
+        <div className="flex-1 text-sm @[16rem]:text-base">
           <Skeleton.Text characterCount={10} className="rounded" />
           <Skeleton.Text characterCount={8} className="rounded" />
           <Skeleton.Text characterCount={6} className="rounded" />
         </div>
       </div>
-    </div>
+    </Skeleton.Root>
   );
 }
