bd66fd3b0d2f8bc78c417197f964415cc9953c62
Hunter Garrett <hunter@hunterbecton.com>
2025-06-06 18:42:26 -0400

    fix: wrong fallback values for vars, docs, and examples

--- apps/web/vibes/soul/examples/form/number-input/index.tsx
+++ apps/web/vibes/soul/examples/form/number-input/index.tsx
@@ -3,13 +3,7 @@ import { NumberInput } from '@/vibes/soul/form/number-input';
 export default function Preview() {
   return (
     <div className="p-10">
-      <NumberInput
-        defaultValue={1}
-        errors={['Please select a quantity.']}
-        label="Quantity"
-        max={5}
-        min={0}
-      />
+      <NumberInput defaultValue={1} label="Quantity" max={5} min={0} />
     </div>
   );
 }
